943a9ab5374f6db785d4fab4e2f982387547e6a7
Eric Helgeson <erichelgeson@gmail.com>
2021-02-01 11:39:50 -0600

    Black code formatting

--- src/web/ractl_cmds.py
+++ src/web/ractl_cmds.py
@@ -4,8 +4,9 @@ import subprocess
 import re
 
 from settings import *
-valid_file_types = ['*.hda', '*.iso', '*.cdr', '*.zip']
-valid_file_types = r'|'.join([fnmatch.translate(x) for x in valid_file_types])
+
+valid_file_types = ["*.hda", "*.iso", "*.cdr", "*.zip"]
+valid_file_types = r"|".join([fnmatch.translate(x) for x in valid_file_types])
 # List of SCSI ID's you'd like to exclude - eg if you are on a Mac, the System is usually 7
 EXCLUDE_SCSI_IDS = [7]
 
@@ -20,19 +21,27 @@ def list_files():
     for path, dirs, files in os.walk(base_dir):
         # Only list valid file types
         files = [f for f in files if re.match(valid_file_types, f)]
-        files_list.extend([
-            (os.path.join(path, file),
-             # TODO: move formatting to template
-             '{:,.0f}'.format(os.path.getsize(os.path.join(path, file)) / float(1 << 20)) + " MB")
-            for file in files])
+        files_list.extend(
+            [
+                (
+                    os.path.join(path, file),
+                    # TODO: move formatting to template
+                    "{:,.0f}".format(
+                        os.path.getsize(os.path.join(path, file)) / float(1 << 20)
+                    )
+                    + " MB",
+                )
+                for file in files
+            ]
+        )
     return files_list
 
 
 def get_valid_scsi_ids(devices):
     invalid_list = EXCLUDE_SCSI_IDS.copy()
     for device in devices:
-        if device['file'] != "NO MEDIA" and device['file'] != "-":
-            invalid_list.append(int(device['id']))
+        if device["file"] != "NO MEDIA" and device["file"] != "-":
+            invalid_list.append(int(device["id"]))
 
     valid_list = list(range(8))
     for id in invalid_list:
@@ -50,7 +59,10 @@ def attach_image(scsi_id, image, type):
     if type == "cd" and get_type(scsi_id) == "SCCD":
         return insert(scsi_id, image)
     else:
-        return subprocess.run(["rasctl", "-c", "attach", "-t", type, "-i", scsi_id, "-f", image], capture_output=True)
+        return subprocess.run(
+            ["rasctl", "-c", "attach", "-t", type, "-i", scsi_id, "-f", image],
+            capture_output=True,
+        )
 
 
 def detach_by_id(scsi_id):
@@ -58,7 +70,9 @@ def detach_by_id(scsi_id):
 
 
 def disconnect_by_id(scsi_id):
-    return subprocess.run(["rasctl", "-c", "disconnect", "-i", scsi_id], capture_output=True)
+    return subprocess.run(
+        ["rasctl", "-c", "disconnect", "-i", scsi_id], capture_output=True
+    )
 
 
 def eject_by_id(scsi_id):
@@ -66,33 +80,45 @@ def eject_by_id(scsi_id):
 
 
 def insert(scsi_id, image):
-    return subprocess.run(["rasctl", "-i", scsi_id, "-c", "insert", "-f", image], capture_output=True)
+    return subprocess.run(
+        ["rasctl", "-i", scsi_id, "-c", "insert", "-f", image], capture_output=True
+    )
 
 
 def rascsi_service(action):
     # start/stop/restart
-    return subprocess.run(["sudo", "/bin/systemctl", action, "rascsi.service"]).returncode == 0
+    return (
+        subprocess.run(["sudo", "/bin/systemctl", action, "rascsi.service"]).returncode
+        == 0
+    )
 
 
 def list_devices():
     device_list = []
     for id in range(8):
         device_list.append({"id": str(id), "un": "-", "type": "-", "file": "-"})
-    output = subprocess.run(["rasctl", "-l"], capture_output=True).stdout.decode("utf-8")
+    output = subprocess.run(["rasctl", "-l"], capture_output=True).stdout.decode(
+        "utf-8"
+    )
     for line in output.splitlines():
         # Valid line to process, continue
-        if not line.startswith("+") and \
-                not line.startswith("| ID |") and \
-                (not line.startswith("No device is installed.") or line.startswith("No images currently attached.")) \
-                and len(line) > 0:
+        if (
+            not line.startswith("+")
+            and not line.startswith("| ID |")
+            and (
+                not line.startswith("No device is installed.")
+                or line.startswith("No images currently attached.")
+            )
+            and len(line) > 0
+        ):
             line.rstrip()
             device = {}
             segments = line.split("|")
             if len(segments) > 4:
                 idx = int(segments[1].strip())
                 device_list[idx]["id"] = str(idx)
-                device_list[idx]['un'] = segments[2].strip()
-                device_list[idx]['type'] = segments[3].strip()
-                device_list[idx]['file'] = segments[4].strip()
+                device_list[idx]["un"] = segments[2].strip()
+                device_list[idx]["type"] = segments[3].strip()
+                device_list[idx]["file"] = segments[4].strip()
 
     return device_list
--- src/web/web.py
+++ src/web/web.py
@@ -3,204 +3,226 @@ import os
 from flask import Flask, render_template, request, flash, url_for, redirect, send_file
 from werkzeug.utils import secure_filename
 
-from file_cmds import create_new_image, download_file_to_iso, delete_image, unzip_file, download_image
+from file_cmds import (
+    create_new_image,
+    download_file_to_iso,
+    delete_image,
+    unzip_file,
+    download_image,
+)
 from pi_cmds import shutdown_pi, reboot_pi, running_version, rascsi_service
-from ractl_cmds import attach_image, list_devices, is_active, list_files, detach_by_id, eject_by_id, get_valid_scsi_ids
+from ractl_cmds import (
+    attach_image,
+    list_devices,
+    is_active,
+    list_files,
+    detach_by_id,
+    eject_by_id,
+    get_valid_scsi_ids,
+)
 from settings import *
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     devices = list_devices()
     scsi_ids = get_valid_scsi_ids(devices)
-    return render_template('index.html',
-                           devices=devices,
-                           active=is_active(),
-                           files=list_files(),
-                           base_dir=base_dir,
-                           scsi_ids=scsi_ids,
-                           max_file_size=MAX_FILE_SIZE,
-                           version=running_version())
+    return render_template(
+        "index.html",
+        devices=devices,
+        active=is_active(),
+        files=list_files(),
+        base_dir=base_dir,
+        scsi_ids=scsi_ids,
+        max_file_size=MAX_FILE_SIZE,
+        version=running_version(),
+    )
 
-@app.route('/logs')
+
+@app.route("/logs")
 def logs():
     import subprocess
-    lines = request.args.get('lines') or "100"
+
+    lines = request.args.get("lines") or "100"
     process = subprocess.run(["journalctl", "-n", lines], capture_output=True)
 
     if process.returncode == 0:
-        headers = { 'content-type':'text/plain' }
+        headers = {"content-type": "text/plain"}
         return process.stdout.decode("utf-8"), 200, headers
     else:
-        flash(u'Failed to get logs')
-        flash(process.stdout.decode("utf-8"), 'stdout')
-        flash(process.stderr.decode("utf-8"), 'stderr')
-        return redirect(url_for('index'))
+        flash(u"Failed to get logs")
+        flash(process.stdout.decode("utf-8"), "stdout")
+        flash(process.stderr.decode("utf-8"), "stderr")
+        return redirect(url_for("index"))
 
 
-@app.route('/scsi/attach', methods=['POST'])
+@app.route("/scsi/attach", methods=["POST"])
 def attach():
-    file_name = request.form.get('file_name')
-    scsi_id = request.form.get('scsi_id')
+    file_name = request.form.get("file_name")
+    scsi_id = request.form.get("scsi_id")
 
     # Validate image type by suffix
-    if file_name.lower().endswith('.iso') or file_name.lower().endswith('iso'):
+    if file_name.lower().endswith(".iso") or file_name.lower().endswith("iso"):
         image_type = "cd"
-    elif file_name.lower().endswith('.hda'):
+    elif file_name.lower().endswith(".hda"):
         image_type = "hd"
     else:
-        flash(u'Unknown file type. Valid files are .iso, .hda, .cdr', 'error')
-        return redirect(url_for('index'))
+        flash(u"Unknown file type. Valid files are .iso, .hda, .cdr", "error")
+        return redirect(url_for("index"))
 
     process = attach_image(scsi_id, file_name, image_type)
     if process.returncode == 0:
-        flash('Attached '+ file_name + " to scsi id " + scsi_id + "!")
-        return redirect(url_for('index'))
+        flash("Attached " + file_name + " to scsi id " + scsi_id + "!")
+        return redirect(url_for("index"))
     else:
-        flash(u'Failed to attach '+ file_name + " to scsi id " + scsi_id + "!", 'error')
-        flash(process.stdout.decode("utf-8"), 'stdout')
-        flash(process.stderr.decode("utf-8"), 'stderr')
-        return redirect(url_for('index'))
+        flash(
+            u"Failed to attach " + file_name + " to scsi id " + scsi_id + "!", "error"
+        )
+        flash(process.stdout.decode("utf-8"), "stdout")
+        flash(process.stderr.decode("utf-8"), "stderr")
+        return redirect(url_for("index"))
 
 
-@app.route('/scsi/detach', methods=['POST'])
+@app.route("/scsi/detach", methods=["POST"])
 def detach():
-    scsi_id = request.form.get('scsi_id')
+    scsi_id = request.form.get("scsi_id")
     process = detach_by_id(scsi_id)
     if process.returncode == 0:
         flash("Detached scsi id " + scsi_id + "!")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        flash(u"Failed to detach scsi id " + scsi_id + "!", 'error')
-        flash(process.stdout, 'stdout')
-        flash(process.stderr, 'stderr')
-        return redirect(url_for('index'))
+        flash(u"Failed to detach scsi id " + scsi_id + "!", "error")
+        flash(process.stdout, "stdout")
+        flash(process.stderr, "stderr")
+        return redirect(url_for("index"))
 
 
-@app.route('/scsi/eject', methods=['POST'])
+@app.route("/scsi/eject", methods=["POST"])
 def eject():
-    scsi_id = request.form.get('scsi_id')
+    scsi_id = request.form.get("scsi_id")
     process = eject_by_id(scsi_id)
     if process.returncode == 0:
         flash("Ejected scsi id " + scsi_id + "!")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        flash(u"Failed to eject scsi id " + scsi_id + "!", 'error')
-        flash(process.stdout, 'stdout')
-        flash(process.stderr, 'stderr')
-        return redirect(url_for('index'))
+        flash(u"Failed to eject scsi id " + scsi_id + "!", "error")
+        flash(process.stdout, "stdout")
+        flash(process.stderr, "stderr")
+        return redirect(url_for("index"))
 
 
-@app.route('/pi/reboot', methods=['POST'])
+@app.route("/pi/reboot", methods=["POST"])
 def restart():
     reboot_pi()
     flash("Restarting...")
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
-@app.route('/rascsi/restart', methods=['POST'])
+@app.route("/rascsi/restart", methods=["POST"])
 def rascsi_restart():
     rascsi_service("restart")
     flash("Restarting RaSCSI Service...")
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
-@app.route('/pi/shutdown', methods=['POST'])
+@app.route("/pi/shutdown", methods=["POST"])
 def shutdown():
     shutdown_pi()
     flash("Shutting down...")
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
-@app.route('/files/download_to_iso', methods=['POST'])
+@app.route("/files/download_to_iso", methods=["POST"])
 def download_file():
-    scsi_id = request.form.get('scsi_id')
-    url = request.form.get('url')
+    scsi_id = request.form.get("scsi_id")
+    url = request.form.get("url")
     process = download_file_to_iso(scsi_id, url)
     if process.returncode == 0:
         flash("File Downloaded")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        flash(u"Failed to download file", 'error')
-        flash(process.stdout, 'stdout')
-        flash(process.stderr, 'stderr')
-        return redirect(url_for('index'))
+        flash(u"Failed to download file", "error")
+        flash(process.stdout, "stdout")
+        flash(process.stderr, "stderr")
+        return redirect(url_for("index"))
 
 
-@app.route('/files/download_image', methods=['POST'])
+@app.route("/files/download_image", methods=["POST"])
 def download_img():
-    url = request.form.get('url')
+    url = request.form.get("url")
     # TODO: error handling
     download_image(url)
     flash("File Downloaded")
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
-@app.route('/files/upload', methods=['POST'])
+@app.route("/files/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
-        flash('No file part', 'error')
-        return redirect(url_for('index'))
-    file = request.files['file']
+    if "file" not in request.files:
+        flash("No file part", "error")
+        return redirect(url_for("index"))
+    file = request.files["file"]
     if file:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('index', filename=filename))
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return redirect(url_for("index", filename=filename))
 
 
-@app.route('/files/create', methods=['POST'])
+@app.route("/files/create", methods=["POST"])
 def create_file():
-    file_name = request.form.get('file_name')
-    size = request.form.get('size')
-    type = request.form.get('type')
+    file_name = request.form.get("file_name")
+    size = request.form.get("size")
+    type = request.form.get("type")
 
     process = create_new_image(file_name, type, size)
     if process.returncode == 0:
         flash("Drive created")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        flash(u"Failed to create file", 'error')
-        flash(process.stdout, 'stdout')
-        flash(process.stderr, 'stderr')
-        return redirect(url_for('index'))
+        flash(u"Failed to create file", "error")
+        flash(process.stdout, "stdout")
+        flash(process.stderr, "stderr")
+        return redirect(url_for("index"))
 
 
-@app.route('/files/download', methods=['POST'])
+@app.route("/files/download", methods=["POST"])
 def download():
-    image = request.form.get('image')
+    image = request.form.get("image")
     return send_file(base_dir + image, as_attachment=True)
 
 
-@app.route('/files/delete', methods=['POST'])
+@app.route("/files/delete", methods=["POST"])
 def delete():
-    image = request.form.get('image')
+    image = request.form.get("image")
     if delete_image(image):
         flash("File " + image + " deleted")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        flash(u"Failed to Delete " + image, 'error')
-        return redirect(url_for('index'))
+        flash(u"Failed to Delete " + image, "error")
+        return redirect(url_for("index"))
 
 
-@app.route('/files/unzip', methods=['POST'])
+@app.route("/files/unzip", methods=["POST"])
 def unzip():
-    image = request.form.get('image')
+    image = request.form.get("image")
 
     if unzip_file(image):
         flash("Unzipped file " + image)
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        flash(u"Failed to unzip " + image, 'error')
-        return redirect(url_for('index'))
+        flash(u"Failed to unzip " + image, "error")
+        return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-    app.secret_key = 'rascsi_is_awesome_insecure_secret_key'
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.config['UPLOAD_FOLDER'] = base_dir
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.config['MAX_CONTENT_LENGTH'] = MAX_FILE_SIZE
+    app.secret_key = "rascsi_is_awesome_insecure_secret_key"
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["UPLOAD_FOLDER"] = base_dir
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
+    app.config["MAX_CONTENT_LENGTH"] = MAX_FILE_SIZE
 
     from waitress import serve
+
     serve(app, host="0.0.0.0", port=8080)
