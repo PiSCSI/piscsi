57b899e21d0c3eea2e6c196e883189f59a4f57ac
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-06-23 11:48:55 +0200

    Removed unrelated changes

--- src/raspberrypi/filepath.h
+++ src/raspberrypi/filepath.h
@@ -15,7 +15,7 @@ class Fileio;
 
 //---------------------------------------------------------------------------
 //
-//	Constant definition
+//	定数定義
 //
 //---------------------------------------------------------------------------
 #define FILEPATH_MAX		_MAX_PATH
@@ -40,10 +40,17 @@ public:
 										// クリア
 	void FASTCALL SetPath(LPCSTR path);
 										// ファイル設定(ユーザ) MBCS用
+	BOOL FASTCALL IsClear() const;
+										// クリアされているか
 	LPCTSTR FASTCALL GetPath() const	{ return m_szPath; }
 										// パス名取得
+	const char* FASTCALL GetShort() const;
+										// ショート名取得(const char*)
 	LPCTSTR FASTCALL GetFileExt() const;
 										// ショート名取得(LPCTSTR)
+	BOOL FASTCALL CmpPath(const Filepath& path) const;
+										// パス比較
+
 	BOOL FASTCALL Save(Fileio *fio, int ver);
 										// セーブ
 	BOOL FASTCALL Load(Fileio *fio, int ver);
@@ -52,8 +59,12 @@ public:
 private:
 	void FASTCALL Split();
 										// パス分割
+	void FASTCALL SetCurDir();
+										// カレントディレクトリ設定
 	TCHAR m_szPath[_MAX_PATH];
 										// ファイルパス
+	TCHAR m_szDrive[_MAX_DRIVE];
+										// ドライブ
 	TCHAR m_szDir[_MAX_DIR];
 										// ディレクトリ
 	TCHAR m_szFile[_MAX_FNAME];
@@ -61,6 +72,8 @@ private:
 	TCHAR m_szExt[_MAX_EXT];
 										// 拡張子
 
+	static char ShortName[_MAX_FNAME + _MAX_DIR];
+										// ショート名(char)
 	static TCHAR FileExt[_MAX_FNAME + _MAX_DIR];
 										// ショート名(TCHAR)
 };
