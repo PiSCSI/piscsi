65d4129d5ced1f923199c3c7794851afcac18e5b
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-07-18 09:39:50 +0200

    Fixed comment

--- src/raspberrypi/rasctl.cpp
+++ src/raspberrypi/rasctl.cpp
@@ -229,7 +229,7 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	// File check (command is ATTACH and type is HD, CD or MO)
+	// File check (command is ATTACH and type is HD)
 	if (cmd == ATTACH && (type == SASI_HD || type == SCSI_HD) && params.empty()) {
 		cerr << "Error : Invalid file path" << endl;
 		exit(EINVAL);
