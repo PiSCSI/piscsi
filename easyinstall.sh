1695b40c6568ba32c5bedf4d3ca6fcbf298d4861
Eric Helgeson <erichelgeson@gmail.com>
2021-07-29 19:04:29 -0500

    fix bridge-utils missing from dev (#157)

--- easyinstall.sh
+++ easyinstall.sh
@@ -41,7 +41,7 @@ function initialChecks() {
 }
 
 function installPackages() {
-    sudo apt-get update && sudo apt install git libspdlog-dev libpcap-dev genisoimage python3 python3-venv nginx libpcap-dev protobuf-compiler -y
+    sudo apt-get update && sudo apt install git libspdlog-dev libpcap-dev genisoimage python3 python3-venv nginx libpcap-dev protobuf-compiler bridge-utils -y
 }
 
 # install all dependency packages for RaSCSI Service
