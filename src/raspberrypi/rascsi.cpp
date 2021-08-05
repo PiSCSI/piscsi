07d2e85b90c18baf487e17f908fe648829fa1b2e
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-08-05 10:57:24 +0200

    Keep log level and report an error if level is invalid, and return a status

--- src/raspberrypi/rascsi.cpp
+++ src/raspberrypi/rascsi.cpp
@@ -431,9 +431,7 @@ bool ReturnStatus(int fd, bool status = true, const string msg = "") {
 	return status;
 }
 
-void SetLogLevel(const string& log_level) {
-	spdlog_log_level = log_level;
-
+bool SetLogLevel(const string& log_level) {
 	if (log_level == "trace") {
 		set_level(level::trace);
 	}
@@ -456,10 +454,12 @@ void SetLogLevel(const string& log_level) {
 		set_level(level::off);
 	}
 	else {
-		LOGWARN("Invalid log level '%s', falling back to 'trace'", log_level.c_str());
-		spdlog_log_level = "trace";
-		set_level(level::trace);
+		return false;
 	}
+
+	spdlog_log_level = log_level;
+
+	return true;
 }
 
 void LogDeviceList(const string& device_list)
@@ -776,7 +776,9 @@ bool ParseArgument(int argc, char* argv[], int& port)
 		id = -1;
 	}
 
-	SetLogLevel(log_level);
+	if (!SetLogLevel(log_level)) {
+		LOGWARN("Invalid log level '%s'", log_level.c_str());
+	}
 
 	// Display and log the device list
 	const PbDevices devices = GetDevices();
@@ -856,6 +858,15 @@ static void *MonThread(void *param)
 			}
 			else if (command.cmd() == LOG_LEVEL) {
 				SetLogLevel(command.params());
+				bool status = SetLogLevel(command.params());
+				if (!status) {
+					ostringstream error;
+					error << "Invalid log level: " << command.params();
+					ReturnStatus(fd, false, error.str());
+				}
+				else {
+					ReturnStatus(fd);
+				}
 			}
 			else if (command.cmd() == SERVER_INFO) {
 				PbServerInfo serverInfo;
@@ -908,6 +919,7 @@ int main(int argc, char* argv[])
 	struct sched_param schparam;
 
 	SetLogLevel("trace");
+
 	// Create a thread-safe stdout logger to process the log messages
 	auto logger = stdout_color_mt("rascsi stdout logger");
 
