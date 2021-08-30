11f9e9cfa2d1af93f8f004d57795714d2affaf40
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-08-30 14:25:44 +0200

    Updated GetEventStatusNotification

--- src/raspberrypi/devices/scsicd.cpp
+++ src/raspberrypi/devices/scsicd.cpp
@@ -747,6 +747,7 @@ void SCSICD::GetEventStatusNotification(SASIDEV *controller)
 	if (!ctrl->cmd[1] & 0x01) {
 		// Asynchronous notification is optional and not supported by rascsi
 		controller->Error(ERROR_CODES::sense_key::ILLEGAL_REQUEST, ERROR_CODES::asc::INVALID_FIELD_IN_CDB);
+		return;
 	}
 
 	LOGTRACE("Received request for event polling, which is currently not supported");
