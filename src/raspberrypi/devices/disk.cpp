6a072598900814f1dfc147ede2a21dd4072c8909
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-09-10 21:33:56 +0200

    Updated status handling

--- src/raspberrypi/devices/disk.cpp
+++ src/raspberrypi/devices/disk.cpp
@@ -1291,9 +1291,6 @@ bool Disk::StartStop(const DWORD *cdb)
 		LOGTRACE("%s", start ? "Starting unit" : "Stopping unit");
 
 		SetStopped(!start);
-		if (!start) {
-			SetReady(false);
-		}
 	}
 
 	// Look at the eject bit and eject if necessary
