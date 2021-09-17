ad0e454e6623a18defbc6c3e9c1d741c422be6a9
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-09-17 20:14:03 +0200

    Simplified filename validation

--- src/raspberrypi/rascsi.cpp
+++ src/raspberrypi/rascsi.cpp
@@ -717,10 +717,6 @@ string SetReservedIds(const string& ids)
 
 bool IsValidFilename(const string& filename)
 {
-	if (filename == "." || filename == "..") {
-		return false;
-	}
-
 	struct stat st;
 	return stat(filename.c_str(), &st) || !S_ISREG(st.st_mode);
 }
