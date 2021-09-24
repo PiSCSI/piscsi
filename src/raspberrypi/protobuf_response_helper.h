829356a32cd49886536249b5a7d62f58dadaa31c
Uwe Seimet <48174652+uweseimet@users.noreply.github.com>
2021-09-24 08:48:48 +0200

    protobuf interface returns assumed device type (#259)
    * Added device type to PbImageFile
    
    * Comment update
    
    * Code cleanup
    
    * More code cleanup
    
    * Extraced methods
    
    * Extracted methods
    
    * Renaming
    
    * Moved code
    
    * Visility update
    
    * Updated error handling
    
    * Removed duplicate error handling
    
    * Removed duplicate code
    
    * Code cleanup
    
    * Moved code
    
    * Moved code

---
+++ src/raspberrypi/protobuf_response_helper.h
@@ -0,0 +1,53 @@
+//---------------------------------------------------------------------------
+//
+// SCSI Target Emulator RaSCSI (*^..^*)
+// for Raspberry Pi
+//
+// Copyright (C) 2021 Uwe Seimet
+//
+// A singleton that creates responses for protobuf interface requests
+//
+//---------------------------------------------------------------------------
+
+#pragma once
+
+#include "devices/device_factory.h"
+#include "rascsi_interface.pb.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace rascsi_interface;
+
+class Device;
+
+class ProtobufResponseHandler
+{
+public:
+
+	ProtobufResponseHandler();
+	~ProtobufResponseHandler() {};
+
+	static ProtobufResponseHandler& instance();
+
+	PbImageFilesInfo *GetAvailableImages(PbResult&, const string&);
+	void GetDevices(PbServerInfo&, const vector<Device *>&, const string&);
+	void GetDevicesInfo(PbResult&, const PbCommand&, const vector<Device *>&, const string&, int);
+	PbDeviceTypesInfo *GetDeviceTypesInfo(PbResult&, const PbCommand&);
+	PbServerInfo *GetServerInfo(PbResult&, const vector<Device *>&, const set<int>&, const string&, const string&);
+	PbNetworkInterfacesInfo *GetNetworkInterfacesInfo(PbResult&);
+
+private:
+
+	DeviceFactory device_factory;
+
+	vector<string> log_levels;
+
+	PbDeviceProperties *GetDeviceProperties(const Device *);
+	void GetDevice(const Device *, PbDevice *, const string&);
+	void GetAllDeviceTypeProperties(PbDeviceTypesInfo&);
+	void GetDeviceTypeProperties(PbDeviceTypesInfo&, PbDeviceType);
+	void GetAvailableImages(PbResult& result, PbServerInfo&, const string&);
+	void GetImageFile(PbImageFile *, const string&, const string&);
+	void GetLogLevels(PbServerInfo&);
+};
