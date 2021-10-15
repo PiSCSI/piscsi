36fd8002c3444f4aa2a53f4323a76f58800f558d
Daniel Markstedt <markstedt@gmail.com>
2021-10-14 22:22:02 -0700

    Define afp_dir

--- src/web/settings.py
+++ src/web/settings.py
@@ -1,7 +1,11 @@
 from os import getenv, getcwd
 
-cfg_dir = getenv("HOME", "/home/pi/") + ".config/rascsi/"
-home_dir = getcwd()
+# TODO: Make home_dir portable when running rascsi-web 
+# as a service, since the HOME env variable doesn't get set then.
+home_dir = getenv("HOME", "/home/pi")
+cfg_dir = f"{home_dir}/.config/rascsi/"
+afp_dir = f"{home_dir}/afpshare/"
+web_dir = getcwd()
 
 DEFAULT_CONFIG = "default.json"
 MAX_FILE_SIZE = getenv("MAX_FILE_SIZE", 1024 * 1024 * 1024 * 4)  # 4gb
@@ -9,7 +13,7 @@ MAX_FILE_SIZE = getenv("MAX_FILE_SIZE", 1024 * 1024 * 1024 * 4)  # 4gb
 ARCHIVE_FILE_SUFFIX = ("zip",)
 
 # File containing canonical drive properties
-DRIVE_PROPERTIES_FILE = home_dir + "/drive_properties.json"
+DRIVE_PROPERTIES_FILE = web_dir + "/drive_properties.json"
 # File ending used for drive properties files
 PROPERTIES_SUFFIX = "properties"
 
