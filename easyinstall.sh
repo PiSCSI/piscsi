863946af12133b47f7edd72b608555dd09138927
Daniel Markstedt <markstedt@gmail.com>
2021-10-16 14:31:21 -0700

    Fix errors

--- easyinstall.sh
+++ easyinstall.sh
@@ -466,10 +466,10 @@ function installNetatalk() {
 	
     cd "netatalk-classic-$NETATALK_VERSION" || exit 1
     sed -i /^~/d ./config/AppleVolumes.default.tmpl
-    echo "/home/pi/afpshare 'Pi File Server' adouble:v1 volcharset:ASCII" >> ./config/AppleVolumes.default.tmpl
+    echo "/home/pi/afpshare \"Pi File Server\" adouble:v1 volcharset:ASCII" >> ./config/AppleVolumes.default.tmpl
 	
     echo "ATALKD_RUN=yes" >> ./config/netatalk.conf
-    echo "'RaSCSI-Pi' -transall -uamlist uams_guest.so,uams_clrtxt.so,uams_dhx.so -defaultvol /etc/netatalk/AppleVolumes.default -systemvol /etc/netatalk/AppleVolumes.system -nosavepassword -nouservol -guestname 'nobody' -setuplog 'default log_maxdebug /var/log/afpd.log'" >> ./config/afpd.conf.tmpl
+    echo "\"RaSCSI-Pi\" -transall -uamlist uams_guest.so,uams_clrtxt.so,uams_dhx.so -defaultvol /etc/netatalk/AppleVolumes.default -systemvol /etc/netatalk/AppleVolumes.system -nosavepassword -nouservol -guestname \"nobody\" -setuplog \"default log_maxdebug /var/log/afpd.log\"" >> ./config/afpd.conf.tmpl
 	
     ( sudo apt-get update && sudo apt-get install libssl-dev libdb-dev libcups2-dev autotools-dev automake libtool --assume-yes ) </dev/null
 
