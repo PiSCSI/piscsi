a125a138df74db5007731eea7262d2d341c1e91b
Daniel Markstedt <markstedt@gmail.com>
2021-10-19 10:45:08 -0700

    Add start script for oled monitor

--- src/oled_monitor/start.sh
+++ src/oled_monitor/start.sh
@@ -2,39 +2,22 @@
 set -e
 # set -x # Uncomment to Debug
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 # verify packages installed
 ERROR=0
-if ! command -v dpkg -l i2c-tools &> /dev/null ; then
-    echo "i2c-tools could not be found"
-    echo "Run 'sudo apt install i2c-tools' to fix."
-    ERROR=1
-fi
 if ! command -v python3 &> /dev/null ; then
     echo "python3 could not be found"
     echo "Run 'sudo apt install python3' to fix."
     ERROR=1
 fi
-# Dep to build Pillow
-if ! dpkg -l python3-dev &> /dev/null; then
-    echo "python3-dev could not be found"
-    echo "Run 'sudo apt install python3-dev' to fix."
-    ERROR=1
-fi
-if ! dpkg -l libjpeg-dev &> /dev/null; then
-    echo "libjpeg-dev could not be found"
-    echo "Run 'sudo apt install libjpeg-dev' to fix."
-    ERROR=1
-fi
-if ! dpkg -l libpng-dev &> /dev/null; then
-    echo "libpng-dev could not be found"
-    echo "Run 'sudo apt install libpng-dev' to fix."
-    ERROR=1
-fi
-# Dep to build Pollow
 if ! python3 -m venv --help &> /dev/null ; then
     echo "venv could not be found"
     echo "Run 'sudo apt install python3-venv' to fix."
+    ERROR=1
+fi
+if ! command -v unzip &> /dev/null ; then
+    echo "unzip could not be found"
+    echo "Run 'sudo apt install unzip' to fix."
     ERROR=1
 fi
 if [ $ERROR = 1 ] ; then
@@ -43,16 +26,13 @@ if [ $ERROR = 1 ] ; then
   exit 1
 fi
 
-if ! i2cdetect -y 1 &> /dev/null ; then
-    echo "i2cdetect -y 1 did not find a screen."
-    exit 2
-fi
 if ! test -e venv; then
-  echo "Creating python venv for OLED Screen"
+  echo "Creating python venv for OLED monitor"
   python3 -m venv venv
   echo "Activating venv"
   source venv/bin/activate
   echo "Installing requirements.txt"
+  pip install wheel
   pip install -r requirements.txt
   git rev-parse HEAD > current
 fi
@@ -65,12 +45,10 @@ if ! test -e current; then
 else
   if [ "$(cat current)" != "$(git rev-parse HEAD)" ]; then
       echo "New version detected, updating requirements.txt"
-      echo " This may take some time..."
-      pip install wheel
       pip install -r requirements.txt
       git rev-parse HEAD > current
   fi
 fi
 
-echo "Starting OLED Screen..."
+echo "Starting OLED monitor..."
 python3 rascsi_oled_monitor.py
