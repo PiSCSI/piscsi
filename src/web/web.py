39bd6b7ee96d5bb3612093991e0f6207ea81cc07
Daniel Markstedt <markstedt@gmail.com>
2021-10-22 06:42:28 -0700

    Bring back manual unzip, remove automatic unzip on upload, and show only relevant controls for zip files

--- src/web/web.py
+++ src/web/web.py
@@ -108,6 +108,7 @@ def index():
         device_types=device_types["device_types"],
         free_disk=int(disk["free"] / 1024 / 1024),
         valid_file_suffix=valid_file_suffix,
+        archive_file_suffix=ARCHIVE_FILE_SUFFIX,
         removable_device_types=REMOVABLE_DEVICE_TYPES,
     )
 
@@ -570,12 +571,9 @@ def upload_file():
             return make_response(("Transferred file corrupted!", 500))
         else:
             log.info(f"File {file.filename} has been uploaded successfully")
-            if filename.lower().endswith(".zip"):
-                unzip_file(filename)
     else:
         log.debug(f"Chunk {current_chunk + 1} of {total_chunks} "
                   f"for file {file.filename} completed.")
-
 
     return make_response(("File upload successful!", 200))
 
@@ -634,6 +632,18 @@ def delete():
             return redirect(url_for("index"))
 
     return redirect(url_for("index"))
+
+
+@app.route("/files/unzip", methods=["POST"])
+def unzip():
+    image = request.form.get("image")
+
+    if unzip_file(image):
+        flash("Unzipped file " + image)
+        return redirect(url_for("index"))
+    else:
+        flash("Failed to unzip " + image, "error")
+        return redirect(url_for("index"))
 
 
 @app.route("/files/prop", methods=["POST"])
