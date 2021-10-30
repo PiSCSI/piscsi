d71ddda1889bff7fbe780b7cbdbaeb83eeea4f2f
Daniel Markstedt <markstedt@gmail.com>
2021-10-30 13:04:49 -0700

    Clean up indentation

--- src/web/start.sh
+++ src/web/start.sh
@@ -26,33 +26,33 @@ if ! command -v unzip &> /dev/null ; then
     ERROR=1
 fi
 if [ $ERROR = 1 ] ; then
-  echo
-  echo "Fix errors and re-run ./start.sh"
-  exit 1
+    echo
+    echo "Fix errors and re-run ./start.sh"
+    exit 1
 fi
 
 if ! test -e venv; then
-  echo "Creating python venv for web server"
-  python3 -m venv venv
-  echo "Activating venv"
-  source venv/bin/activate
-  echo "Installing requirements.txt"
-  pip install wheel
-  pip install -r requirements.txt
-  git rev-parse HEAD > current
+    echo "Creating python venv for web server"
+    python3 -m venv venv
+    echo "Activating venv"
+    source venv/bin/activate
+    echo "Installing requirements.txt"
+    pip install wheel
+    pip install -r requirements.txt
+    git rev-parse HEAD > current
 fi
 
 source venv/bin/activate
 
 # Detect if someone updates - we need to re-run pip install.
 if ! test -e current; then
-  git rev-parse > current
+    git rev-parse > current
 else
-  if [ "$(cat current)" != "$(git rev-parse HEAD)" ]; then
-      echo "New version detected, updating requirements.txt"
-      pip install -r requirements.txt
-      git rev-parse HEAD > current
-  fi
+    if [ "$(cat current)" != "$(git rev-parse HEAD)" ]; then
+        echo "New version detected, updating requirements.txt"
+        pip install -r requirements.txt
+        git rev-parse HEAD > current
+    fi
 fi
 
 echo "Starting web server..."
