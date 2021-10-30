a07d0d4969cfb5d81496e552eda131d0fbe112da
Daniel Markstedt <markstedt@gmail.com>
2021-10-30 16:52:44 -0700

    Move non-rascsi command methods into its own file

---
+++ src/web/device_utils.py
@@ -0,0 +1,45 @@
+def get_valid_scsi_ids(devices, reserved_ids):
+    """
+    Takes a list of dicts devices, and list of ints reserved_ids.
+    Returns:
+    - list of ints valid_ids, which are the SCSI ids that are not reserved
+    - int recommended_id, which is the id that the Web UI should default to recommend
+    """
+    occupied_ids = []
+    for d in devices:
+        occupied_ids.append(d["id"])
+
+    unoccupied_ids = [i for i in list(range(8)) if i not in reserved_ids + occupied_ids]
+    unoccupied_ids.sort()
+    valid_ids = [i for i in list(range(8)) if i not in reserved_ids]
+    valid_ids.sort(reverse=True)
+
+    if len(unoccupied_ids) > 0:
+        recommended_id = unoccupied_ids[-1]
+    else:
+        recommended_id = occupied_ids.pop(0)
+
+    return valid_ids, recommended_id
+
+
+def sort_and_format_devices(devices):
+    """
+    Takes a list of dicts devices and returns a list of dicts.
+    Sorts by SCSI ID acending (0 to 7).
+    For SCSI IDs where no device is attached, inject a dict with placeholder text.
+    """
+    occupied_ids = []
+    for d in devices:
+        occupied_ids.append(d["id"])
+
+    formatted_devices = devices
+
+    # Add padding devices and sort the list
+    for id in range(8):
+        if id not in occupied_ids:
+            formatted_devices.append({"id": id, "device_type": "-", \
+                    "status": "-", "file": "-", "product": "-"})
+    # Sort list of devices by id
+    formatted_devices.sort(key=lambda dic: str(dic["id"]))
+
+    return formatted_devices
