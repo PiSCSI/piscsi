088e7951f139971b07810cc574f214dc85990cb2
Daniel Markstedt <markstedt@gmail.com>
2021-11-08 18:55:04 -0800

    Inline attaching of removable media in the device list (#421)
    * Clean up constant name
    
    * Inline removable media insertion
    
    * Add missing code
    
    * Revert mistaken change

--- src/web/web.py
+++ src/web/web.py
@@ -134,6 +134,9 @@ def index():
         device_types=device_types["device_types"],
         free_disk=int(disk["free"] / 1024 / 1024),
         valid_file_suffix=valid_file_suffix,
+        cdrom_file_suffix=tuple(server_info["sccd"]),
+        removable_file_suffix=tuple(server_info["scrm"]),
+        mo_file_suffix=tuple(server_info["scmo"]),
         archive_file_suffix=ARCHIVE_FILE_SUFFIX,
         REMOVABLE_DEVICE_TYPES=REMOVABLE_DEVICE_TYPES,
     )
