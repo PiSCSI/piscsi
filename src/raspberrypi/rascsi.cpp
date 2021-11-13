522b41498064482ed9fe560adc25909810debed3
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-11-13 11:55:01 +0100

    Include file cleanup

--- src/raspberrypi/rascsi.cpp
+++ src/raspberrypi/rascsi.cpp
@@ -12,15 +12,11 @@
 
 #include "rascsi.h"
 #include "os.h"
-#include "filepath.h"
-#include "fileio.h"
 #include "controllers/scsidev_ctrl.h"
 #include "controllers/sasidev_ctrl.h"
 #include "devices/device_factory.h"
 #include "devices/device.h"
 #include "devices/disk.h"
-#include "devices/scsi_host_bridge.h"
-#include "devices/scsi_daynaport.h"
 #include "devices/file_support.h"
 #include "gpiobus.h"
 #include "exceptions.h"
@@ -32,16 +28,12 @@
 #include "rascsi_interface.pb.h"
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
-#include <spdlog/async.h>
-#include <dirent.h>
-#include <ifaddrs.h>
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <list>
 #include <vector>
 #include <map>
-#include <filesystem>
 
 using namespace std;
 using namespace spdlog;
