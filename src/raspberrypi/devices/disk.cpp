48ec76a423e7495811d7c5c42a2944f0102ef1b4
Uwe Seimet <Uwe.Seimet@seimet.de>
2021-11-24 11:43:42 +0100

    Updated dispatcher table

--- src/raspberrypi/devices/disk.cpp
+++ src/raspberrypi/devices/disk.cpp
@@ -51,8 +51,8 @@ Disk::Disk(const std::string id) : Device(id), ScsiPrimaryCommands(), ScsiBlockC
 	AddCommand(SCSIDEV::eCmdReadCapacity10, "ReadCapacity10", &Disk::ReadCapacity10);
 	AddCommand(SCSIDEV::eCmdRead10, "Read10", &Disk::Read10);
 	AddCommand(SCSIDEV::eCmdWrite10, "Write10", &Disk::Write10);
-	AddCommand(SCSIDEV::eCmdReadLong10, "ReadLong10", &Disk::ReadWriteLong10);
-	AddCommand(SCSIDEV::eCmdWriteLong10, "WriteLong10", &Disk::ReadWriteLong10);
+	AddCommand(SCSIDEV::eCmdReadLong10, "ReadLong10", &Disk::ReadLong10);
+	AddCommand(SCSIDEV::eCmdWriteLong10, "WriteLong10", &Disk::WriteLong10);
 	AddCommand(SCSIDEV::eCmdSeek10, "Seek10", &Disk::Seek10);
 	AddCommand(SCSIDEV::eCmdVerify10, "Verify10", &Disk::Verify10);
 	AddCommand(SCSIDEV::eCmdSynchronizeCache10, "SynchronizeCache10", &Disk::SynchronizeCache10);
