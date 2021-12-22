62101d8f296acfc8cd92bd20ff594c86a0e8c82e
Daniel Markstedt <markstedt@gmail.com>
2021-12-21 18:49:56 -0800

    Cleanup

--- easyinstall.sh
+++ easyinstall.sh
@@ -85,7 +85,7 @@ function compileRaScsi() {
     cd "$BASE/src/raspberrypi" || exit 1
 
     echo "Compiling with ${CORES:-1} simultaneous cores..."
-    #( make clean && make -j "${CORES:-1}" all CONNECT_TYPE="${CONNECT_TYPE:-FULLSPEC}" ) </dev/null
+    ( make clean && make -j "${CORES:-1}" all CONNECT_TYPE="${CONNECT_TYPE:-FULLSPEC}" ) </dev/null
 }
 
 # install the RaSCSI binaries and modify the service configuration
