13187fc2fc2fcffb84e4fab1fc2868ecd1546501
Daniel Markstedt <markstedt@gmail.com>
2021-12-27 13:21:56 -0800

    Make language selectable in UI (#568)

--- src/web/file_cmds.py
+++ src/web/file_cmds.py
@@ -5,7 +5,7 @@ Module for methods reading from and writing to the file system
 import os
 import logging
 from pathlib import PurePath
-from flask import current_app
+from flask import current_app, session
 from flask_babel import _
 
 from ractl_cmds import (
@@ -66,7 +66,7 @@ def list_images():
     command = proto.PbCommand()
     command.operation = proto.PbOperation.DEFAULT_IMAGE_FILES_INFO
     command.params["token"] = current_app.config["TOKEN"]
-    command.params["locale"] = current_app.config["LOCALE"]
+    command.params["locale"] = session["language"]
 
     data = send_pb_command(command.SerializeToString())
     result = proto.PbResult()
@@ -123,7 +123,7 @@ def create_new_image(file_name, file_type, size):
     command = proto.PbCommand()
     command.operation = proto.PbOperation.CREATE_IMAGE
     command.params["token"] = current_app.config["TOKEN"]
-    command.params["locale"] = current_app.config["LOCALE"]
+    command.params["locale"] = session["language"]
 
     command.params["file"] = file_name + "." + file_type
     command.params["size"] = str(size)
@@ -144,7 +144,7 @@ def delete_image(file_name):
     command = proto.PbCommand()
     command.operation = proto.PbOperation.DELETE_IMAGE
     command.params["token"] = current_app.config["TOKEN"]
-    command.params["locale"] = current_app.config["LOCALE"]
+    command.params["locale"] = session["language"]
 
     command.params["file"] = file_name
 
@@ -163,7 +163,7 @@ def rename_image(file_name, new_file_name):
     command = proto.PbCommand()
     command.operation = proto.PbOperation.RENAME_IMAGE
     command.params["token"] = current_app.config["TOKEN"]
-    command.params["locale"] = current_app.config["LOCALE"]
+    command.params["locale"] = session["language"]
 
     command.params["from"] = file_name
     command.params["to"] = new_file_name
