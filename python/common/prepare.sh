036bc0fa82b166af339a7c7f80cbc8bf4b9a3cec
Benjamin Zeiss <ben@zeiss.net>
2022-01-16 16:08:13 +0100

    removed superfluous file. #455

--- python/common/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-pybabel extract -F babel.cfg src/rascsi -o src/rascsi/translations/messages.pot
-#pybabel init -i src/rascsi/translations/messages.pot -d src/rascsi/translations -l de
-pybabel compile -d src/rascsi/translations
-#pybabel compile -d src/translations
