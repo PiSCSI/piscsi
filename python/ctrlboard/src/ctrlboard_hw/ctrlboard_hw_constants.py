dac748663ac0de416079ebc0764b9edaf0852f2d
Benjamin Zeiss <ben@zeiss.net>
2022-01-23 18:24:07 +0100

    added i2c ctrlboard detection + error handling. #382

--- python/ctrlboard/src/ctrlboard_hw/ctrlboard_hw_constants.py
+++ python/ctrlboard/src/ctrlboard_hw/ctrlboard_hw_constants.py
@@ -1,5 +1,5 @@
 class CtrlBoardHardwareConstants:
-    SSD1306_I2C_ADDRESS = 0x3c
+    DISPLAY_I2C_ADDRESS = 0x3c
     PCA9554_I2C_ADDRESS = 0x3f
     PCA9554_PIN_ENC_A = 0
     PCA9554_PIN_ENC_B = 1
@@ -10,8 +10,7 @@ class CtrlBoardHardwareConstants:
     PCA9554_PIN_LED_1 = 6
     PCA9554_PIN_LED_2 = 7
 
-    PI_PIN_INTERRUPT = 9   ### BCM
-#    PI_PIN_INTERRUPT = 21  ### Board
+    PI_PIN_INTERRUPT = 9   # BCM
 
     BUTTON_1 = "Bt1"
     BUTTON_2 = "Bt2"
