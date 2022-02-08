c5d45ca6703601a2aec5d19a7f9f97af848cab5a
Daniel Markstedt <markstedt@gmail.com>
2022-02-07 18:21:34 -0800

    Improve the device type selection UI

--- python/common/src/rascsi/common_settings.py
+++ python/common/src/rascsi/common_settings.py
@@ -7,6 +7,7 @@ from os import getcwd
 WORK_DIR = getcwd()
 
 REMOVABLE_DEVICE_TYPES = ("SCCD", "SCRM", "SCMO")
+STANDALONE_DEVICE_TYPES = ("SCBR", "SCDP")
 
 # There may be a more elegant way to get the HOME dir of the user that installed RaSCSI
 HOME_DIR = "/".join(WORK_DIR.split("/")[0:3])
