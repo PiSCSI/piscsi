34f9ed125c5687bec136ad6d992367a1a88b9cd6
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-02-15 21:44:00 +0100

    Updated SCSI level

--- src/raspberrypi/devices/primary_device.cpp
+++ src/raspberrypi/devices/primary_device.cpp
@@ -184,7 +184,7 @@ int PrimaryDevice::Inquiry(int type, bool is_removable, const DWORD *cdb, BYTE *
 		memset(buf, 0, allocation_length);
 		buf[0] = type;
 		buf[1] = is_removable ? 0x80 : 0x00;
-		buf[2] = 0x01;
+		buf[2] = 0x02;
 		buf[4] = 0x1F;
 
 		// Padded vendor, product, revision
