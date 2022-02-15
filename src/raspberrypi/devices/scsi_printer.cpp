c2b0971c13f056b36c4c9d6e1710eb26a28173a4
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-02-15 11:48:41 +0100

    Comment update

--- src/raspberrypi/devices/scsi_printer.cpp
+++ src/raspberrypi/devices/scsi_printer.cpp
@@ -14,7 +14,8 @@
 //
 // 1. The client reserves the printer device with RESERVER UNIT (optional step, mandatory for
 // a multi-initiator environment).
-// 2. The client sends the data to be printed with one or several PRINT commands.
+// 2. The client sends the data to be printed with one or several PRINT commands. Due to
+// https://github.com/akuker/RASCSI/issues/669 the maximum transfer size is limited to 4096 bytes.
 // 3. The client triggers printing with SYNCHRONIZE BUFFERS.
 // 4. The client releases the printer with RELEASE UNIT (optional step, mandatory for
 // a multi-initiator environment).
