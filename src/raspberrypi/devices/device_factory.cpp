af41f215c2bd89c3f6d1a5cfd33004cd98f4d93e
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-02-27 22:00:57 +0100

    Removed comment

--- src/raspberrypi/devices/device_factory.cpp
+++ src/raspberrypi/devices/device_factory.cpp
@@ -31,7 +31,6 @@ DeviceFactory::DeviceFactory()
 	sector_sizes[SCHD] = { 512, 1024, 2048, 4096 };
 	sector_sizes[SCRM] = { 512, 1024, 2048, 4096 };
 	sector_sizes[SCMO] = { 512, 1024, 2048, 4096 };
-	// Some old Sun CD-ROM drives support 512 bytes per sector
 	sector_sizes[SCCD] = { 512, 2048};
 
 	// 128 MB, 512 bytes per sector, 248826 sectors
