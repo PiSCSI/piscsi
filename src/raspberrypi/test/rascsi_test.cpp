2a3c018b3ea91e464106cab9f6da572144334be5
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-18 23:11:43 +0200

    Test update

--- src/raspberrypi/test/rascsi_test.cpp
+++ src/raspberrypi/test/rascsi_test.cpp
@@ -11,14 +11,19 @@ public:
 	SCSIHDMock(const unordered_set<uint32_t>& sector_sizes) : SCSIHD(sector_sizes, false) { };
 	~SCSIHDMock() { };
 
-	// Make method visible for testing
+	// Make protected methods visible for testing
+
+	int AddModePages(const DWORD *cdb, BYTE *buf, int max_length) {
+		return ModePageDevice::AddModePages(cdb, buf, max_length);
+	}
+
 	void AddModePages(map<int, vector<BYTE>>& pages, int page, bool changeable) const override
 	{
 		SCSIHD::AddModePages(pages, page, changeable);
 	}
 };
 
-TEST(ModePageDeviceTest, AddModePages)
+TEST(ModePagesTest, SCSIHD_AddModePages)
 {
 	unordered_set<uint32_t> sector_sizes;
 	map<int, vector<BYTE>> mode_pages;
@@ -26,4 +31,18 @@ TEST(ModePageDeviceTest, AddModePages)
 	SCSIHDMock hd_device(sector_sizes);
 	hd_device.AddModePages(mode_pages, 0x3f, false);
 	EXPECT_EQ(mode_pages.size(), 5);
+}
+
+TEST(ModePagesTest, ModePageDevice_AddModePages)
+{
+	unordered_set<uint32_t> sector_sizes;
+	DWORD cdb[6];
+	BYTE buf[512];
+
+	SCSIHDMock hd_device(sector_sizes);
+	cdb[2] = 0x3f;
+	// Buffer can hold all mode page data
+	EXPECT_EQ(hd_device.AddModePages(cdb, buf, 512), 102);
+	// Allocation length is limited
+	EXPECT_EQ(hd_device.AddModePages(cdb, buf, 1), 1);
 }
