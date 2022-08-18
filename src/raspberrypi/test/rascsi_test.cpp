72f9ac9f24962c046cef404979dc523021a8f2cc
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-18 22:22:22 +0200

    Makefile update

--- src/raspberrypi/test/rascsi_test.cpp
+++ src/raspberrypi/test/rascsi_test.cpp
@@ -2,7 +2,24 @@
 
 #include <gtest/gtest.h>
 
-TEST(ModePageDviceTest, AddModePage)
-{
+using namespace std;
 
+class ModePageDeviceMock: public ModePageDevice
+{
+public:
+	ModePageDeviceMock() : ModePageDevice("test")  { };
+	~ModePageDeviceMock() { };
+
+	vector<BYTE> Inquiry() const override { return inquiry_data; }
+	int ModeSense6(const DWORD *, BYTE *) override { return 0; }
+	int ModeSense10(const DWORD *, BYTE *, int) override { return 0; }
+	void AddModePages(map<int, vector<BYTE>>&, int, bool) const override { };
+
+private:
+	vector<BYTE> inquiry_data;
+};
+
+TEST(ModePageDeviceTest, AddModePages)
+{
+	new ModePageDeviceMock();
 }
