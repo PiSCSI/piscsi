466904d95b442e6c92d01ca8c47afbcaa0da73a7
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-18 22:57:52 +0200

    Test update

--- src/raspberrypi/test/rascsi_test.cpp
+++ src/raspberrypi/test/rascsi_test.cpp
@@ -1,35 +1,29 @@
-#include "../devices/mode_page_device.h"
+#include "../devices/scsihd.h"
 
 #include <gtest/gtest.h>
 
 using namespace std;
 
-class ModePageDeviceMock: public ModePageDevice
+class SCSIHDMock : public SCSIHD
 {
 public:
-	ModePageDeviceMock() : ModePageDevice("test")  { };
-	~ModePageDeviceMock() { };
 
-	vector<BYTE> Inquiry() const override { return inquiry_data; }
-	int ModeSense6(const DWORD *, BYTE *) override { return 0; }
-	int ModeSense10(const DWORD *, BYTE *, int) override { return 0; }
-	void AddModePages(map<int, vector<BYTE>>&, int, bool) const override { };
+	SCSIHDMock(const unordered_set<uint32_t>& sector_sizes) : SCSIHD(sector_sizes, false) { };
+	~SCSIHDMock() { };
 
-	// Required to make protected method accessible
-	int AddModePages(const DWORD *cdb, BYTE *buf, int max_length) {
-		return ModePageDevice::AddModePages(cdb, buf, max_length);
+	// Make method visible for testing
+	void AddModePages(map<int, vector<BYTE>>& pages, int page, bool changeable) const override
+	{
+		SCSIHD::AddModePages(pages, page, changeable);
 	}
-
-private:
-	vector<BYTE> inquiry_data;
 };
 
 TEST(ModePageDeviceTest, AddModePages)
 {
-	ModePageDeviceMock device;
-	DWORD cdb[6];
-	BYTE buf[512];
+	unordered_set<uint32_t> sector_sizes;
+	map<int, vector<BYTE>> mode_pages;
 
-	cdb[2] = 0x3f;
-	device.AddModePages(cdb, buf, 512);
+	SCSIHDMock hd_device(sector_sizes);
+	hd_device.AddModePages(mode_pages, 0x3f, false);
+	EXPECT_EQ(mode_pages.size(), 5);
 }
