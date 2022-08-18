688bb358068b1a6e0c2f4259fbb12ba78b81338d
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-18 22:32:51 +0200

    Make protected method visible

--- src/raspberrypi/test/rascsi_test.cpp
+++ src/raspberrypi/test/rascsi_test.cpp
@@ -15,11 +15,21 @@ public:
 	int ModeSense10(const DWORD *, BYTE *, int) override { return 0; }
 	void AddModePages(map<int, vector<BYTE>>&, int, bool) const override { };
 
+	// Required to make protected method accessible
+	int AddModePages(const DWORD *cdb, BYTE *buf, int max_length) {
+		return ModePageDevice::AddModePages(cdb, buf, max_length);
+	}
+
 private:
 	vector<BYTE> inquiry_data;
 };
 
 TEST(ModePageDeviceTest, AddModePages)
 {
-	new ModePageDeviceMock();
+	ModePageDeviceMock device;
+	DWORD cdb[6];
+	BYTE buf[512];
+
+	cdb[2] = 0x3f;
+	device.AddModePages(cdb, buf, 512);
 }
