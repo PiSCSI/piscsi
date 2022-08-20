bbb1ebbf01f82f6b5f9560e55fc9b30f67b1e53e
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-20 09:00:36 +0200

    Merged develop

--- easyinstall.sh
+++ easyinstall.sh
@@ -82,7 +82,7 @@ function sudoCheck() {
 
 # install all dependency packages for RaSCSI Service
 function installPackages() {
-    sudo apt-get update && sudo apt-get install git libspdlog-dev libpcap-dev genisoimage python3 python3-venv python3-dev python3-pip nginx libpcap-dev protobuf-compiler bridge-utils libev-dev libevdev2 -y </dev/null
+    sudo apt-get update && sudo apt-get install git libspdlog-dev libpcap-dev genisoimage python3 python3-venv python3-dev python3-pip nginx libpcap-dev protobuf-compiler bridge-utils libev-dev libevdev2 unar -y </dev/null
 }
 
 # cache the pip packages
