edf99df6cc263c42b35ece9c8744c3df49be0b53
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-20 16:33:55 +0200

    Added test

---
+++ src/raspberrypi/test/device_factory_test.cpp
@@ -0,0 +1,39 @@
+//---------------------------------------------------------------------------
+//
+// SCSI Target Emulator RaSCSI (*^..^*)
+// for Raspberry Pi
+//
+// Copyright (C) 2022 Uwe Seimet
+//
+// Unit tests based on GoogleTest and GoogleMock
+//
+//---------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "../devices/device_factory.h"
+
+using namespace std;
+using namespace rascsi_interface;
+
+DeviceFactory& device_factory = DeviceFactory::instance();
+
+TEST(DeviceFactoryTest, GetExtension)
+{
+	EXPECT_EQ(SAHD, device_factory.GetTypeForFile("test.hdf"));
+	EXPECT_EQ(SCHD, device_factory.GetTypeForFile("test.hds"));
+	EXPECT_EQ(SCHD, device_factory.GetTypeForFile("test.HDS"));
+	EXPECT_EQ(SCHD, device_factory.GetTypeForFile("test.hda"));
+	EXPECT_EQ(SCHD, device_factory.GetTypeForFile("test.hdn"));
+	EXPECT_EQ(SCHD, device_factory.GetTypeForFile("test.hdi"));
+	EXPECT_EQ(SCHD, device_factory.GetTypeForFile("test.nhd"));
+	EXPECT_EQ(SCRM, device_factory.GetTypeForFile("test.hdr"));
+	EXPECT_EQ(SCMO, device_factory.GetTypeForFile("test.mos"));
+	EXPECT_EQ(SCCD, device_factory.GetTypeForFile("test.iso"));
+	EXPECT_EQ(SCBR, device_factory.GetTypeForFile("bridge"));
+	EXPECT_EQ(SCDP, device_factory.GetTypeForFile("daynaport"));
+	EXPECT_EQ(SCLP, device_factory.GetTypeForFile("printer"));
+	EXPECT_EQ(SCHS, device_factory.GetTypeForFile("services"));
+	EXPECT_EQ(UNDEFINED, device_factory.GetTypeForFile("unknown"));
+}
