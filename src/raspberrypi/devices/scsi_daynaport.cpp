67575cf396e5fc0f3daef67e4fa3d097f676fef3
Daniel Markstedt <markstedt@gmail.com>
2022-08-22 21:50:55 -0700

    Rebrand to RaSCSI Reloaded in C++ codebase

--- src/raspberrypi/devices/scsi_daynaport.cpp
+++ src/raspberrypi/devices/scsi_daynaport.cpp
@@ -1,6 +1,6 @@
 //---------------------------------------------------------------------------
 //
-//	SCSI Target Emulator RaSCSI (*^..^*)
+//	SCSI Target Emulator RaSCSI Reloaded
 //	for Raspberry Pi
 //
 //  Copyright (C) 2020 akuker
