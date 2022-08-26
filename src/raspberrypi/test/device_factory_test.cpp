36dafb15554ab4c022954f6d03c29aa2a0dab251
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-26 17:14:39 +0200

    Moved code

--- src/raspberrypi/test/device_factory_test.cpp
+++ src/raspberrypi/test/device_factory_test.cpp
@@ -11,6 +11,8 @@
 
 #include <gtest/gtest.h>
 
+#include "rascsi_version.h"
+#include "../devices/device.h"
 #include "../devices/device_factory.h"
 
 using namespace rascsi_interface;
@@ -39,6 +41,203 @@ TEST(DeviceFactoryTest, GetTypeForFile)
 	EXPECT_EQ(device_factory.GetTypeForFile("services"), SCHS);
 	EXPECT_EQ(device_factory.GetTypeForFile("unknown"), UNDEFINED);
 	EXPECT_EQ(device_factory.GetTypeForFile("test.iso.suffix"), UNDEFINED);
+}
+
+TEST(DeviceTest, UnknownDeviceType)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "test");
+	EXPECT_EQ(nullptr, device);
+}
+
+TEST(DeviceTest, SCHD_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "test.hda");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(true, device->SupportsFile());
+	EXPECT_EQ(false, device->SupportsParams());
+	EXPECT_EQ(true, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(false, device->IsReadOnly());
+	EXPECT_EQ(false, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(false, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(true, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("QUANTUM", device->GetVendor()) << "Invalid default vendor for Apple drive";
+	EXPECT_EQ("FIREBALL", device->GetProduct()) << "Invalid default vendor for Apple drive";
+	EXPECT_EQ(string(rascsi_get_version_string()).substr(0, 2) + string(rascsi_get_version_string()).substr(3, 2),
+			device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
+}
+
+TEST(DeviceTest, SCRM_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "test.hdr");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(true, device->SupportsFile());
+	EXPECT_EQ(false, device->SupportsParams());
+	EXPECT_EQ(true, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(false, device->IsReadOnly());
+	EXPECT_EQ(true, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(true, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(true, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("RaSCSI", device->GetVendor());
+	EXPECT_EQ("SCSI HD (REM.)", device->GetProduct());
+	EXPECT_EQ(string(rascsi_get_version_string()).substr(0, 2) + string(rascsi_get_version_string()).substr(3, 2),
+			device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
+}
+
+TEST(DeviceTest, SCMO_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "test.mos");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(true, device->SupportsFile());
+	EXPECT_EQ(false, device->SupportsParams());
+	EXPECT_EQ(true, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(false, device->IsReadOnly());
+	EXPECT_EQ(true, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(true, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(true, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("RaSCSI", device->GetVendor());
+	EXPECT_EQ("SCSI MO", device->GetProduct());
+	EXPECT_EQ(string(rascsi_get_version_string()).substr(0, 2) + string(rascsi_get_version_string()).substr(3, 2),
+			device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
+}
+
+TEST(DeviceTest, SCCD_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "test.iso");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(true, device->SupportsFile());
+	EXPECT_EQ(false, device->SupportsParams());
+	EXPECT_EQ(false, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(true, device->IsReadOnly());
+	EXPECT_EQ(true, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(true, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(true, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("RaSCSI", device->GetVendor());
+	EXPECT_EQ("SCSI CD-ROM", device->GetProduct());
+	EXPECT_EQ(string(rascsi_get_version_string()).substr(0, 2) + string(rascsi_get_version_string()).substr(3, 2),
+			device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
+}
+
+TEST(DeviceTest, SCBR_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "bridge");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(false, device->SupportsFile());
+	EXPECT_EQ(true, device->SupportsParams());
+	EXPECT_EQ(false, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(false, device->IsReadOnly());
+	EXPECT_EQ(false, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(false, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(false, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("RaSCSI", device->GetVendor());
+	EXPECT_EQ("SCSI HOST BRIDGE", device->GetProduct());
+	EXPECT_EQ(string(rascsi_get_version_string()).substr(0, 2) + string(rascsi_get_version_string()).substr(3, 2),
+			device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
+}
+
+TEST(DeviceTest, SCDP_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "daynaport");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(false, device->SupportsFile());
+	EXPECT_EQ(true, device->SupportsParams());
+	EXPECT_EQ(false, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(false, device->IsReadOnly());
+	EXPECT_EQ(false, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(false, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(false, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("Dayna", device->GetVendor());
+	EXPECT_EQ("SCSI/Link", device->GetProduct());
+	EXPECT_EQ("1.4a", device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
+}
+
+TEST(DeviceTest, SCHS_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "services");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(false, device->SupportsFile());
+	EXPECT_EQ(false, device->SupportsParams());
+	EXPECT_EQ(false, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(false, device->IsReadOnly());
+	EXPECT_EQ(false, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(false, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(false, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("RaSCSI", device->GetVendor());
+	EXPECT_EQ("Host Services", device->GetProduct());
+	EXPECT_EQ(string(rascsi_get_version_string()).substr(0, 2) + string(rascsi_get_version_string()).substr(3, 2),
+			device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
+}
+
+TEST(DeviceTest, SCLP_Device_Defaults)
+{
+	Device *device = device_factory.CreateDevice(UNDEFINED, "printer");
+	EXPECT_NE(nullptr, device);
+	EXPECT_EQ(false, device->SupportsFile());
+	EXPECT_EQ(true, device->SupportsParams());
+	EXPECT_EQ(false, device->IsProtectable());
+	EXPECT_EQ(false, device->IsProtected());
+	EXPECT_EQ(false, device->IsReadOnly());
+	EXPECT_EQ(false, device->IsRemovable());
+	EXPECT_EQ(false, device->IsRemoved());
+	EXPECT_EQ(false, device->IsLockable());
+	EXPECT_EQ(false, device->IsLocked());
+	EXPECT_EQ(false, device->IsStoppable());
+	EXPECT_EQ(false, device->IsStopped());
+
+	EXPECT_EQ("RaSCSI", device->GetVendor());
+	EXPECT_EQ("SCSI PRINTER", device->GetProduct());
+	EXPECT_EQ(string(rascsi_get_version_string()).substr(0, 2) + string(rascsi_get_version_string()).substr(3, 2),
+			device->GetRevision());
+
+	EXPECT_EQ(32, device->GetSupportedLuns());
 }
 
 }
