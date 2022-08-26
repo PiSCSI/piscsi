111600f67186e7c5b558b90cc90b2549a8d287db
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-08-26 17:05:01 +0200

    Merge with develop

--- python/common/src/util/unarchiver.py
+++ python/common/src/util/unarchiver.py
@@ -79,12 +79,8 @@ def extract_archive(file_path, **kwargs):
             extracted_members = []
 
             for line in lines[1:-1]:
-<<<<<<< HEAD
-                if line_matches := match(unar_file_extracted, line):
-=======
                 line_matches = match(unar_file_extracted, line)
                 if line_matches:
->>>>>>> develop
                     matches = line_matches.groupdict()
                     member = {
                         "name": str(pathlib.PurePath(matches["path"]).name),
@@ -95,15 +91,11 @@ def extract_archive(file_path, **kwargs):
                         "absolute_path": str(pathlib.PurePath(tmp_dir).joinpath(matches["path"])),
                         }
 
-<<<<<<< HEAD
-                    member_types = matches.get("types", "").removeprefix(", ").split(", ")
-=======
                     member_types = matches.get("types", "")
                     if member_types.startswith(", "):
                         member_types = member_types[2:].split(", ")
                     else:
                         member_types = member_types.split(", ")
->>>>>>> develop
 
                     if "dir" in member_types:
                         member["is_dir"] = True
