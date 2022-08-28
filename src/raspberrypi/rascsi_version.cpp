b5ddb88e6a718684bcb2c65f9c831ef557fbbf5d
Tony Kuker <akuker@gmail.com>
2022-08-28 22:19:06 +0100

    Update revision number for release 22.08.01

--- src/raspberrypi/rascsi_version.cpp
+++ src/raspberrypi/rascsi_version.cpp
@@ -14,7 +14,7 @@
 // The following should be updated for each release
 const int rascsi_major_version = 22; // Last two digits of year
 const int rascsi_minor_version = 8; // Month
-const int rascsi_patch_version = -1;  // Patch number - increment for each update
+const int rascsi_patch_version = 1;  // Patch number - increment for each update
 
 static char rascsi_version_string[30]; // Allow for string up to "XX.XX.XXX" + null character + "development build"
 
