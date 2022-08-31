69e78448912945818563dcc819ab1488fbd8ec01
akuker <34318535+akuker@users.noreply.github.com>
2022-08-31 10:47:56 -0500

    Update easyinstall.sh

--- easyinstall.sh
+++ easyinstall.sh
@@ -82,7 +82,9 @@ function sudoCheck() {
 
 # install all dependency packages for RaSCSI Service
 function installPackages() {
-    sudo apt-get update && sudo apt-get install git libspdlog-dev libpcap-dev genisoimage python3 python3-venv python3-dev python3-pip nginx libpcap-dev protobuf-compiler bridge-utils libev-dev libevdev2 unar -y </dev/null
+    sudo apt-get update && sudo apt-get install git libspdlog-dev libpcap-dev \
+    	genisoimage python3 python3-venv python3-dev python3-pip nginx libpcap-dev \
+	protobuf-compiler bridge-utils libev-dev libevdev2 unar disktype -y </dev/null
 }
 
 # cache the pip packages
