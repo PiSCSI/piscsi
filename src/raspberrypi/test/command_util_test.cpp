469f7869c71dedcb5ea581d9954314e22fa15c9f
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-09-22 09:02:32 +0200

    Added unit test

--- src/raspberrypi/test/command_util_test.cpp
+++ src/raspberrypi/test/command_util_test.cpp
@@ -40,8 +40,24 @@ TEST(CommandUtil, ParseParameters)
 	ParseParameters(device2, "a");
 	ASSERT_EQ("a", GetParam(device2, "file"));
 	ASSERT_EQ("a", GetParam(device2, "interfaces"));
+
 	PbDeviceDefinition device3;
 	ParseParameters(device3, "bridge");
 	ASSERT_EQ("bridge", GetParam(device3, "file"));
 	ASSERT_EQ("", GetParam(device3, "interfaces"));
+
+	PbDeviceDefinition device4;
+	ParseParameters(device4, "daynaport");
+	ASSERT_EQ("daynaport", GetParam(device4, "file"));
+	ASSERT_EQ("", GetParam(device4, "interfaces"));
+
+	PbDeviceDefinition device5;
+	ParseParameters(device5, "printer");
+	ASSERT_EQ("printer", GetParam(device5, "file"));
+	ASSERT_EQ("", GetParam(device5, "interfaces"));
+
+	PbDeviceDefinition device6;
+	ParseParameters(device6, "services");
+	ASSERT_EQ("services", GetParam(device6, "file"));
+	ASSERT_EQ("", GetParam(device6, "interfaces"));
 }
