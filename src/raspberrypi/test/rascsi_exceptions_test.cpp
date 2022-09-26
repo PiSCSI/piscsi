bb4e04f4469b22d425a2381683305cda94b4766b
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-09-26 19:12:15 +0200

    Added tests

--- src/raspberrypi/test/rascsi_exceptions_test.cpp
+++ src/raspberrypi/test/rascsi_exceptions_test.cpp
@@ -35,6 +35,33 @@ TEST(RascsiExceptionsTest, IoException)
 TEST(RascsiExceptionsTest, ScsiErrorException)
 {
 	try {
+		throw scsi_error_exception();
+	}
+	catch(const scsi_error_exception& e) {
+		EXPECT_EQ(sense_key::ABORTED_COMMAND, e.get_sense_key());
+		EXPECT_EQ(asc::NO_ADDITIONAL_SENSE_INFORMATION, e.get_asc());
+		EXPECT_EQ(status::CHECK_CONDITION, e.get_status());
+	}
+
+	try {
+		throw scsi_error_exception(sense_key::UNIT_ATTENTION);
+	}
+	catch(const scsi_error_exception& e) {
+		EXPECT_EQ(sense_key::UNIT_ATTENTION, e.get_sense_key());
+		EXPECT_EQ(asc::NO_ADDITIONAL_SENSE_INFORMATION, e.get_asc());
+		EXPECT_EQ(status::CHECK_CONDITION, e.get_status());
+	}
+
+	try {
+		throw scsi_error_exception(sense_key::UNIT_ATTENTION, asc::LBA_OUT_OF_RANGE);
+	}
+	catch(const scsi_error_exception& e) {
+		EXPECT_EQ(sense_key::UNIT_ATTENTION, e.get_sense_key());
+		EXPECT_EQ(asc::LBA_OUT_OF_RANGE, e.get_asc());
+		EXPECT_EQ(status::CHECK_CONDITION, e.get_status());
+	}
+
+	try {
 		throw scsi_error_exception(sense_key::UNIT_ATTENTION, asc::LBA_OUT_OF_RANGE, status::BUSY);
 	}
 	catch(const scsi_error_exception& e) {
