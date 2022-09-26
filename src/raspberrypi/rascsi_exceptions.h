bb87ed7053a66972af6b957b604c7a51aaf2b9eb
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-09-26 21:38:49 +0200

    Formatting cleanup

--- src/raspberrypi/rascsi_exceptions.h
+++ src/raspberrypi/rascsi_exceptions.h
@@ -13,28 +13,31 @@
 #include <exception>
 #include <string>
 
-class io_exception : public std::exception {
-private:
+class io_exception : public std::exception
+{
 	string msg;
 
 public:
+
 	explicit io_exception(const string& msg) : msg(msg) {}
 	~io_exception() override = default;
 
 	const string& get_msg() const { return msg; }
 };
 
-class file_not_found_exception : public io_exception {
+class file_not_found_exception : public io_exception
+{
 	using io_exception::io_exception;
 };
 
-class scsi_error_exception final : public std::exception {
-private:
+class scsi_error_exception final : public std::exception
+{
 	scsi_defs::sense_key sense_key;
 	scsi_defs::asc asc;
 	scsi_defs::status status;
 
 public:
+
 	scsi_error_exception(scsi_defs::sense_key sense_key = scsi_defs::sense_key::ABORTED_COMMAND,
 			scsi_defs::asc asc = scsi_defs::asc::NO_ADDITIONAL_SENSE_INFORMATION,
 			scsi_defs::status status = scsi_defs::status::CHECK_CONDITION)
