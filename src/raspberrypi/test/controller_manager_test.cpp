e38841d0b06fe0b693f7be97858380762bc3b4a4
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-09-28 12:53:05 +0200

    Test update

--- src/raspberrypi/test/controller_manager_test.cpp
+++ src/raspberrypi/test/controller_manager_test.cpp
@@ -14,27 +14,35 @@
 TEST(ControllerManagerTest, LifeCycle)
 {
 	const int ID = 4;
-	const int LUN = 6;
+	const int LUN1 = 2;
+	const int LUN2 = 3;
 	DeviceFactory device_factory;
 	ControllerManager controller_manager;
 	MockBus bus;
 
-	auto device = device_factory.CreateDevice(UNDEFINED, "services", ID);
-	device->SetId(ID);
-	device->SetLun(LUN);
-
-	controller_manager.CreateScsiController(bus, device);
+	auto device1 = device_factory.CreateDevice(UNDEFINED, "services", ID);
+	device1->SetId(ID);
+	device1->SetLun(LUN1);
+	controller_manager.CreateScsiController(bus, device1);
+	auto controller = controller_manager.FindController(ID);
+	EXPECT_NE(nullptr, controller);
+	EXPECT_EQ(1, controller->GetLunCount());
 	EXPECT_NE(nullptr, controller_manager.IdentifyController(1 << ID));
 	EXPECT_EQ(nullptr, controller_manager.IdentifyController(0));
-	EXPECT_NE(nullptr, controller_manager.FindController(ID));
 	EXPECT_EQ(nullptr, controller_manager.FindController(0));
-	EXPECT_EQ(device, controller_manager.GetDeviceByIdAndLun(ID, LUN));
+	EXPECT_EQ(device1, controller_manager.GetDeviceByIdAndLun(ID, LUN1));
 	EXPECT_EQ(nullptr, controller_manager.GetDeviceByIdAndLun(0, 0));
+
+	auto device2 = device_factory.CreateDevice(UNDEFINED, "services", ID);
+	device2->SetId(ID);
+	device2->SetLun(LUN2);
+	controller_manager.CreateScsiController(bus, device2);
+	EXPECT_FALSE(controller_manager.DeleteController(ID));
 
 	controller_manager.DeleteAllControllers();
 	device_factory.DeleteAllDevices();
 	EXPECT_EQ(nullptr, controller_manager.FindController(ID));
-	EXPECT_EQ(nullptr, controller_manager.GetDeviceByIdAndLun(ID, LUN));
+	EXPECT_EQ(nullptr, controller_manager.GetDeviceByIdAndLun(ID, LUN1));
 }
 
 TEST(ControllerManagerTest, ResetAllControllers)
