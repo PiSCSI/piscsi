c23c3ec96e286edba5da9f145d6526af1fbc7931
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-10-01 23:10:42 +0200

    Removed duplicate test code

--- src/raspberrypi/test/rascsi_response_test.cpp
+++ src/raspberrypi/test/rascsi_response_test.cpp
@@ -27,14 +27,14 @@ TEST(RascsiResponseTest, Operation_Count)
 	EXPECT_EQ(PbOperation_ARRAYSIZE - 1, operation_info->operations_size());
 }
 
-TEST(RascsiResponseTest, GetDevice_Printer)
+void TestNonDiskDevice(const string& name, int default_param_count)
 {
 	MockBus bus;
 	ControllerManager controller_manager(bus);
 	DeviceFactory device_factory;
 	RascsiResponse rascsi_response(device_factory, controller_manager, 32);
 
-	auto d = device_factory.CreateDevice(controller_manager, UNDEFINED, 0, "printer");
+	auto d = device_factory.CreateDevice(controller_manager, UNDEFINED, 0, name);
 	controller_manager.AttachToScsiController(0, d);
 
 	PbServerInfo server_info;
@@ -42,47 +42,33 @@ TEST(RascsiResponseTest, GetDevice_Printer)
 
 	EXPECT_EQ(1, server_info.devices_info().devices().size());
 	const auto& device = server_info.devices_info().devices()[0];
-	EXPECT_EQ(32, device.properties().luns());
-	EXPECT_EQ(0, device.block_size());
-	EXPECT_EQ(0, device.block_count());
-	EXPECT_FALSE(device.properties().supports_file());
-	EXPECT_TRUE(device.properties().supports_params());
-	EXPECT_EQ(2, device.properties().default_params().size());
 	EXPECT_FALSE(device.properties().read_only());
 	EXPECT_FALSE(device.properties().protectable());
 	EXPECT_FALSE(device.properties().stoppable());
 	EXPECT_FALSE(device.properties().removable());
 	EXPECT_FALSE(device.properties().lockable());
 	EXPECT_EQ(0, device.params().size());
+	EXPECT_EQ(32, device.properties().luns());
+	EXPECT_EQ(0, device.block_size());
+	EXPECT_EQ(0, device.block_count());
+	EXPECT_EQ(default_param_count, device.properties().default_params().size());
+	EXPECT_FALSE(device.properties().supports_file());
+	if (!default_param_count) {
+		EXPECT_FALSE(device.properties().supports_file());
+	}
+	else {
+		EXPECT_TRUE(device.properties().supports_params());
+	}
+}
+
+TEST(RascsiResponseTest, GetDevice_Printer)
+{
+	TestNonDiskDevice("printer", 2);
 }
 
 TEST(RascsiResponseTest, GetDevice_HostServices)
 {
-	MockBus bus;
-	ControllerManager controller_manager(bus);
-	DeviceFactory device_factory;
-	RascsiResponse rascsi_response(device_factory, controller_manager, 32);
-
-	auto d = device_factory.CreateDevice(controller_manager, UNDEFINED, 0, "services");
-	controller_manager.AttachToScsiController(0, d);
-
-	PbServerInfo server_info;
-	rascsi_response.GetDevices(server_info, "image_folder");
-
-	EXPECT_EQ(1, server_info.devices_info().devices().size());
-	const auto& device = server_info.devices_info().devices()[0];
-	EXPECT_EQ(32, device.properties().luns());
-	EXPECT_EQ(0, device.block_size());
-	EXPECT_EQ(0, device.block_count());
-	EXPECT_FALSE(device.properties().supports_file());
-	EXPECT_FALSE(device.properties().supports_params());
-	EXPECT_EQ(0, device.properties().default_params().size());
-	EXPECT_FALSE(device.properties().read_only());
-	EXPECT_FALSE(device.properties().protectable());
-	EXPECT_FALSE(device.properties().stoppable());
-	EXPECT_FALSE(device.properties().removable());
-	EXPECT_FALSE(device.properties().lockable());
-	EXPECT_EQ(0, device.params().size());
+	TestNonDiskDevice("services", 0);
 }
 
 TEST(RascsiResponseTest, GetReservedIds)
