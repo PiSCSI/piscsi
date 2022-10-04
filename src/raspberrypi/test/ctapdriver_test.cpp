931983dd57c66f34a0fe0861be94a68a1fb72fca
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-10-04 13:25:00 +0200

    Fixed issues

--- src/raspberrypi/test/ctapdriver_test.cpp
+++ src/raspberrypi/test/ctapdriver_test.cpp
@@ -12,30 +12,30 @@
 
 TEST(CTapDriverTest, Crc32)
 {
-	BYTE buf[ETH_FRAME_LEN];
+	array<BYTE, ETH_FRAME_LEN> buf;
 
-	memset(buf, 0x00, sizeof(buf));
-	EXPECT_EQ(0xe3d887bb, CTapDriver::Crc32(buf, ETH_FRAME_LEN));
+	buf.fill(0x00);
+	EXPECT_EQ(0xe3d887bb, CTapDriver::Crc32(buf.data(), ETH_FRAME_LEN));
 
-	memset(buf, 0xff, sizeof(buf));
-	EXPECT_EQ(0x814765f4, CTapDriver::Crc32(buf, ETH_FRAME_LEN));
+	buf.fill(0xff);
+	EXPECT_EQ(0x814765f4, CTapDriver::Crc32(buf.data(), ETH_FRAME_LEN));
 
-	memset(buf, 0x10, sizeof(buf));
-	EXPECT_EQ(0xb7288Cd3, CTapDriver::Crc32(buf, ETH_FRAME_LEN));
+	buf.fill(0x10);
+	EXPECT_EQ(0xb7288Cd3, CTapDriver::Crc32(buf.data(), ETH_FRAME_LEN));
 
-	memset(buf, 0x7f, sizeof(buf));
-	EXPECT_EQ(0x4b543477, CTapDriver::Crc32(buf, ETH_FRAME_LEN));
+	buf.fill(0x7f);
+	EXPECT_EQ(0x4b543477, CTapDriver::Crc32(buf.data(), ETH_FRAME_LEN));
 
-	memset(buf, 0x80, sizeof(buf));
-	EXPECT_EQ(0x29cbd638, CTapDriver::Crc32(buf, ETH_FRAME_LEN));
+	buf.fill(0x80);
+	EXPECT_EQ(0x29cbd638, CTapDriver::Crc32(buf.data(), ETH_FRAME_LEN));
 
-	for (size_t i = 0; i < sizeof(buf); i++) {
-		buf[i] = i;
+	for (size_t i = 0; i < buf.size(); i++) {
+		buf[i] = (BYTE)i;
 	}
-	EXPECT_EQ(0xe7870705, CTapDriver::Crc32(buf, ETH_FRAME_LEN));
+	EXPECT_EQ(0xe7870705, CTapDriver::Crc32(buf.data(), ETH_FRAME_LEN));
 
-	for (size_t i = sizeof(buf) - 1; i > 0; i--) {
-		buf[i] = i;
+	for (size_t i = buf.size() - 1; i > 0; i--) {
+		buf[i] = (BYTE)i;
 	}
-	EXPECT_EQ(0xe7870705, CTapDriver::Crc32(buf, ETH_FRAME_LEN));
+	EXPECT_EQ(0xe7870705, CTapDriver::Crc32(buf.data(), ETH_FRAME_LEN));
 }
