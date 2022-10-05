9be260459baf66f79985bb85381b7a5643873dd4
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-10-05 12:10:49 +0200

    Added const

--- src/raspberrypi/rascsi/rascsi_service.cpp
+++ src/raspberrypi/rascsi/rascsi_service.cpp
@@ -118,7 +118,7 @@ PbCommand RascsiService::ReadCommand(CommandContext& context) const
 
 	// Read magic string
 	vector<byte> magic(6);
-	if (size_t bytes_read = context.GetSerializer().ReadBytes(fd, magic);
+	if (const size_t bytes_read = context.GetSerializer().ReadBytes(fd, magic);
 		bytes_read != magic.size() || memcmp(magic.data(), "RASCSI", magic.size())) {
 		throw io_exception("Invalid magic");
 	}
