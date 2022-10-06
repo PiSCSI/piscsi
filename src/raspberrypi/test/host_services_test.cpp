18f05b9e08c0a1617e2f83ee4832eb47a7cce081
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-10-06 23:27:36 +0200

    Added unit test

--- src/raspberrypi/test/host_services_test.cpp
+++ src/raspberrypi/test/host_services_test.cpp
@@ -47,6 +47,74 @@ TEST(HostServicesTest, StartStopUnit)
     EXPECT_THROW(device->Dispatch(scsi_command::eCmdStartStop), scsi_error_exception);
 }
 
+TEST(HostServicesTest, ModeSense6)
+{
+	MockBus bus;
+	ControllerManager controller_manager(bus);
+    MockAbstractController controller(0);
+	auto device = make_shared<MockHostServices>(1, controller_manager);
+
+    controller.AddDevice(device);
+
+    vector<int>& cmd = controller.InitCmd(6);
+
+    EXPECT_THROW(device->Dispatch(scsi_command::eCmdModeSense6), scsi_error_exception)
+    	<< "Unsupported mode page was returned";
+
+    cmd[2] = 0x20;
+    EXPECT_THROW(device->Dispatch(scsi_command::eCmdModeSense6), scsi_error_exception)
+    	<< "Block descriptors are not supported";
+
+    cmd[1] = 0x08;
+    // ALLOCATION LENGTH
+    cmd[4] = 255;
+    EXPECT_CALL(controller, DataIn()).Times(1);
+    EXPECT_TRUE(device->Dispatch(scsi_command::eCmdModeSense6));
+	const vector<BYTE>& buffer = controller.GetBuffer();
+	// Major version 1
+	EXPECT_EQ(0x01, buffer[6]);
+	// Minor version 0
+	EXPECT_EQ(0x00, buffer[7]);
+	// Year
+	EXPECT_NE(0x00, buffer[9]);
+	// Day
+	EXPECT_NE(0x00, buffer[10]);
+}
+
+TEST(HostServicesTest, ModeSense10)
+{
+	MockBus bus;
+	ControllerManager controller_manager(bus);
+    MockAbstractController controller(0);
+	auto device = make_shared<MockHostServices>(1, controller_manager);
+
+    controller.AddDevice(device);
+
+    vector<int>& cmd = controller.InitCmd(10);
+
+    EXPECT_THROW(device->Dispatch(scsi_command::eCmdModeSense10), scsi_error_exception)
+    	<< "Unsupported mode page was returned";
+
+    cmd[2] = 0x20;
+    EXPECT_THROW(device->Dispatch(scsi_command::eCmdModeSense10), scsi_error_exception)
+    	<< "Block descriptors are not supported";
+
+    cmd[1] = 0x08;
+    // ALLOCATION LENGTH
+    cmd[8] = 255;
+    EXPECT_CALL(controller, DataIn()).Times(1);
+    EXPECT_TRUE(device->Dispatch(scsi_command::eCmdModeSense10));
+	const vector<BYTE>& buffer = controller.GetBuffer();
+	// Major version 1
+	EXPECT_EQ(0x01, buffer[10]);
+	// Minor version 0
+	EXPECT_EQ(0x00, buffer[11]);
+	// Year
+	EXPECT_NE(0x00, buffer[13]);
+	// Day
+	EXPECT_NE(0x00, buffer[14]);
+}
+
 TEST(HostServicesTest, SetUpModePages)
 {
 	MockBus bus;
