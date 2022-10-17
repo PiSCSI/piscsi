2025c0be6b265dab46b26499516c8e43744d3aed
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-10-17 11:58:58 +0200

    Added unit teste

---
+++ src/raspberrypi/test/localizer_test.cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------
+//
+// SCSI Target Emulator RaSCSI Reloaded
+// for Raspberry Pi
+//
+// Copyright (C) 2022 Uwe Seimet
+//
+//---------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include "rascsi/localizer.h"
+
+TEST(Localizer, Localize)
+{
+	Localizer localizer;
+
+	string message = localizer.Localize(LocalizationKey::ERROR_AUTHENTICATION, "");
+	EXPECT_FALSE(message.empty());
+	EXPECT_EQ(string::npos, message.find("enum value"));
+
+	message = localizer.Localize((LocalizationKey)1234, "");
+	EXPECT_FALSE(message.empty());
+	EXPECT_NE(string::npos, message.find("enum value"));
+}
