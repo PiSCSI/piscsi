c1c751b12663568b3ff5648716e6e22ff82c806d
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-10-18 12:33:45 +0200

    Unit test update

--- src/raspberrypi/test/scsimo_test.cpp
+++ src/raspberrypi/test/scsimo_test.cpp
@@ -8,9 +8,6 @@
 //---------------------------------------------------------------------------
 
 #include "mocks.h"
-#include "devices/scsi_command_util.h"
-
-using namespace scsi_command_util;
 
 TEST(ScsiMoTest, Inquiry)
 {
@@ -32,38 +29,4 @@ TEST(ScsiMoTest, SetUpModePages)
 	EXPECT_EQ(4, mode_pages[6].size());
 	EXPECT_EQ(12, mode_pages[8].size());
 	EXPECT_EQ(12, mode_pages[32].size());
-}
-
-TEST(ScsiMoTest, ModeSense6)
-{
-	NiceMock<MockAbstractController> controller(0);
-	const unordered_set<uint32_t> sector_sizes;
-	auto mo = make_shared<MockSCSIMO>(0, sector_sizes);
-
-	controller.AddDevice(mo);
-
-	vector<int>& cmd = controller.GetCmd();
-
-	cmd[2] = 0x3f;
-	// ALLOCATION LENGTH
-	cmd[4] = 255;
-	EXPECT_TRUE(mo->Dispatch(scsi_command::eCmdModeSense6));
-	EXPECT_EQ(100, controller.GetBuffer()[0]) << "Wrong data length";
-}
-
-TEST(ScsiMoTest, ModeSense10)
-{
-	NiceMock<MockAbstractController> controller(0);
-	const unordered_set<uint32_t> sector_sizes;
-	auto mo = make_shared<MockSCSIMO>(0, sector_sizes);
-
-	controller.AddDevice(mo);
-
-	vector<int>& cmd = controller.GetCmd();
-
-	cmd[2] = 0x3f;
-	// ALLOCATION LENGTH
-	cmd[8] = 255;
-	EXPECT_TRUE(mo->Dispatch(scsi_command::eCmdModeSense10));
-	EXPECT_EQ(96, GetInt16(controller.GetBuffer(), 0)) << "Wrong data length";
 }
