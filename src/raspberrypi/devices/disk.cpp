478fcf99048b3a40d5f213e6379f49f0a3665ff3
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-10-20 17:07:35 +0200

    Rejected read/write access if the drive has 0 sectors

--- src/raspberrypi/devices/disk.cpp
+++ src/raspberrypi/devices/disk.cpp
@@ -691,7 +691,7 @@ bool Disk::CheckAndGetStartAndCount(uint64_t& start, uint32_t& count, access_mod
 	LOGTRACE("%s READ/WRITE/VERIFY/SEEK command record=$%08X blocks=%d", __PRETTY_FUNCTION__, (uint32_t)start, count)
 
 	// Check capacity
-	if (uint64_t capacity = GetBlockCount(); start > capacity || start + count > capacity) {
+	if (uint64_t capacity = GetBlockCount(); !capacity || start > capacity || start + count > capacity) {
 		LOGTRACE("%s", ("Capacity of " + to_string(capacity) + " block(s) exceeded: Trying to access block "
 				+ to_string(start) + ", block count " + to_string(count)).c_str())
 		throw scsi_exception(sense_key::ILLEGAL_REQUEST, asc::LBA_OUT_OF_RANGE);
