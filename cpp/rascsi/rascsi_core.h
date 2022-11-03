edb8a2f081938dfac13484b9d6e362f7d242a501
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-11-03 08:32:09 +0100

    Reduced using directives

--- cpp/rascsi/rascsi_core.h
+++ cpp/rascsi/rascsi_core.h
@@ -18,8 +18,7 @@ using namespace std;
 
 class Rascsi
 {
-	using optarg_value_type = pair<int, string>;
-	using optarg_queue_type = vector<optarg_value_type>;
+	using optarg_queue_type = vector<pair<int, string>>;
 
 	static const int DEFAULT_PORT = 6868;
 	static const char COMPONENT_SEPARATOR = ':';
