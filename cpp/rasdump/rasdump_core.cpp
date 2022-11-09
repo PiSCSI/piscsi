b807d3c109106f53f8603fb9cb140facbd4f0823
Uwe Seimet <Uwe.Seimet@seimet.de>
2022-11-09 16:06:11 +0100

    Fixed issue

--- cpp/rasdump/rasdump_core.cpp
+++ cpp/rasdump/rasdump_core.cpp
@@ -173,9 +173,9 @@ void RasDump::Command(scsi_command cmd, vector<uint8_t>& cdb) const
 
 	WaitPhase(BUS::phase_t::command);
 
-	// Send command. Success if the transmission result is the same as the number of requests
 	cdb[0] = static_cast<uint8_t>(cmd);
-	cdb[1] |= static_cast<uint8_t>(static_cast<byte>(target_lun << 5));
+	const byte lun = static_cast<byte>(cdb[1]) | static_cast<byte>(target_lun << 5);
+	cdb[1] = static_cast<uint8_t>(lun);
 	if (static_cast<int>(cdb.size()) != bus->SendHandShake(cdb.data(), static_cast<int>(cdb.size()), BUS::SEND_NO_DELAY)) {
 		BusFree();
 
