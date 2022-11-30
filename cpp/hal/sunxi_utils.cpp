d1dfa68165b6654593f7a2e3438957fe688600b8
Tony Kuker <akuker@gmail.com>
2022-11-29 18:45:39 -0600

    add RPI.GPIO copyright notice

--- cpp/hal/sunxi_utils.cpp
+++ cpp/hal/sunxi_utils.cpp
@@ -10,6 +10,28 @@
 //  This should include generic functions that can be applicable to
 //  different variants of the SunXI (Allwinner) SoCs
 //
+//  Large portions of this functionality were derived from c_gpio.c, which
+//  is part of the RPI.GPIO library available here:
+//     https://github.com/BPI-SINOVOIP/RPi.GPIO/blob/master/source/c_gpio.c
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+//  of the Software, and to permit persons to whom the Software is furnished to do
+//  so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
 //---------------------------------------------------------------------------
 
 #include "hal/sunxi_utils.h"
