792adaa9b9ef2555b317e1965b4cbdb5466b184a
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-09-15 17:43:07 +0200

    Update handling of logging during unit tests

--- cpp/test/test_setup.cpp
+++ cpp/test/test_setup.cpp
@@ -13,10 +13,24 @@
 
 int main(int argc, char *[])
 {
+	const bool disable_logging = argc <= 1;
+
 	// If any argument is provided the log level is set to trace
-	spdlog::set_level(argc > 1 ? spdlog::level::trace : spdlog::level::off);
+	spdlog::set_level(disable_logging ? spdlog::level::off : spdlog::level::trace);
+
+	int fd = -1;
+	if (disable_logging) {
+		fd = open("/dev/null", O_WRONLY);
+		dup2(fd, STDERR_FILENO);
+	}
 
 	testing::InitGoogleTest();
 
-	return RUN_ALL_TESTS();
+	const int result = RUN_ALL_TESTS();
+
+	if (fd != -1) {
+		close(fd);
+	}
+
+	return result;
 }
