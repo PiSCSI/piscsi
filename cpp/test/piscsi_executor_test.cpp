844ef6fe00429caf1dcc6ba0c81a2e01fedfc143
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-09-15 11:35:43 +0200

    Remove obsolete include

--- cpp/test/piscsi_executor_test.cpp
+++ cpp/test/piscsi_executor_test.cpp
@@ -7,7 +7,6 @@
 //
 //---------------------------------------------------------------------------
 
-#include "spdlog/spdlog.h"
 #include "mocks.h"
 #include "shared/protobuf_util.h"
 #include "shared/piscsi_exceptions.h"
