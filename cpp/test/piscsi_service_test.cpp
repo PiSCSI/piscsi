99803d9928c4d96aa3be48ac9175aa26af3887bb
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-09-18 12:15:53 +0200

    Add unit test

--- cpp/test/piscsi_service_test.cpp
+++ cpp/test/piscsi_service_test.cpp
@@ -11,7 +11,16 @@
 
 #include <gtest/gtest.h>
 
+#include "generated/piscsi_interface.pb.h"
+#include "shared/network_util.h"
+#include "shared/protobuf_serializer.h"
+#include "piscsi/command_context.h"
 #include "piscsi/piscsi_service.h"
+#include <unistd.h>
+#include <netdb.h>
+
+using namespace piscsi_interface;
+using namespace network_util;
 
 TEST(PiscsiServiceTest, Init)
 {
@@ -32,4 +41,35 @@ TEST(PiscsiServiceTest, StartStop)
 
 	service.Start();
 	service.Stop();
+}
+
+TEST(PiscsiServiceTest, Execute)
+{
+	sockaddr_in server_addr = {};
+	EXPECT_TRUE(ResolveHostName("127.0.0.1", &server_addr));
+
+	const int fd = socket(AF_INET, SOCK_STREAM, 0);
+	ASSERT_NE(-1, fd);
+
+	server_addr.sin_port = htons(uint16_t(9999));
+	EXPECT_FALSE(connect(fd, (sockaddr *)&server_addr, sizeof(server_addr)) >= 0) << "Service should not be running";
+
+	PiscsiService service;
+	service.Init([] (const CommandContext& context) { PbResult result; context.WriteResult(result); return true; }, 9999);
+	service.Start();
+	EXPECT_TRUE(connect(fd, (sockaddr *)&server_addr, sizeof(server_addr)) >= 0) << "Service should be running";
+
+	ASSERT_EQ(6, write(fd, "RASCSI", 6));
+
+	PbCommand command;
+	PbResult result;
+	const ProtobufSerializer serializer;
+	serializer.SerializeMessage(fd, command);
+    serializer.DeserializeMessage(fd, result);
+
+    close(fd);
+
+    EXPECT_FALSE(result.status());
+
+    service.Stop();
 }
