9530722e02ef3535ce552b7e750af21b3c54ab11
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-01 11:43:59 +0200

    Update comment

--- cpp/piscsi/piscsi_response.cpp
+++ cpp/piscsi/piscsi_response.cpp
@@ -5,7 +5,7 @@
 //
 // Copyright (C) 2021-2023 Uwe Seimet
 //
-// This class uses raw pointers because protobuf does not have support for smart pointers
+// This class uses raw pointers for protobuf structures because protobuf does not have support for smart pointers
 //
 //---------------------------------------------------------------------------
 
