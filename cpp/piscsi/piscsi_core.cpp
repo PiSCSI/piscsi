b1728b6153262bf873cf17c7b48332617387c2c7
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-01 18:35:14 +0200

    Cleanup

--- cpp/piscsi/piscsi_core.cpp
+++ cpp/piscsi/piscsi_core.cpp
@@ -440,14 +440,13 @@ bool Piscsi::ExecuteCommand(const CommandContext& context)
 		// The remaining commands can only be executed when the target is idle
 		// TODO What happens when the target becomes active while the command is still being executed?
 		// A field 'mutex locker' can probably avoid SCSI commands and ProcessCmd() being executed at the same time
-		default: {
+		default:
 			// TODO Find a better way to wait
 			const timespec ts = { .tv_sec = 0, .tv_nsec = 500'000'000};
 			while (target_is_active) {
 				nanosleep(&ts, nullptr);
 			}
 			return executor->ProcessCmd(context);
-		}
 	}
 
 	return true;
