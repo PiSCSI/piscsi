70ab42b6814d6b241856a1f8a98827dadcf2d3bc
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-02 12:09:15 +0200

    Use hasher

--- cpp/devices/device.h
+++ cpp/devices/device.h
@@ -18,7 +18,7 @@ using namespace piscsi_interface;
 
 using id_set = pair<int, int>;
 
-using param_map = unordered_map<string, string>;
+using param_map = unordered_map<string, string, hash<string>>;
 
 class Device //NOSONAR The number of fields and methods is justified, the complexity is low
 {
