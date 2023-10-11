df140ff63f383efeedf978ba53066c9259a295c3
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-11 20:56:00 +0200

    Merge with develop

--- easyinstall.sh
+++ easyinstall.sh
@@ -48,7 +48,13 @@ echo -e $logo
 
 CONNECT_TYPE="FULLSPEC"
 COMPILER="clang++"
-CORES=1
+MEM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+CORES=`expr $MEM / 450000`
+if [ $CORES -gt $(nproc) ]; then
+        CORES=$(nproc)
+elif [ $CORES -lt 1 ]; then
+        CORES=1
+fi
 USER=$(whoami)
 BASE=$(dirname "$(readlink -f "${0}")")
 CPP_PATH="$BASE/cpp"
