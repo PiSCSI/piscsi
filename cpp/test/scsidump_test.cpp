d6cb3c6e63a824922019e438fb58cba63ff83520
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-22 17:33:59 +0200

    Added options to only run INQUIRY and to scan the bus to scsidump

--- cpp/test/scsidump_test.cpp
+++ cpp/test/scsidump_test.cpp
@@ -1,72 +1,66 @@
-
 //---------------------------------------------------------------------------
 //
 // SCSI Target Emulator PiSCSI
 // for Raspberry Pi
 //
 // Copyright (C) 2022 akuker
+// Copyright (C) 2023 Uwe Seimet
 //
 //---------------------------------------------------------------------------
 
-#include "mocks.h"
+#include <gtest/gtest.h>
+
 #include "scsidump/scsidump_core.h"
 #include "test/test_shared.h"
-#include <filesystem>
-#include <fstream>
 
 using namespace std;
 using namespace filesystem;
 
-class TestableScsidump : public ScsiDump
-{
-  public:
-    static void PublicGeneratePropertiesFile(const string& filename, const inquiry_info_t& inq_info)
-    {
-        ScsiDump::GeneratePropertiesFile(filename, inq_info);
-    }
-};
-
 TEST(ScsiDumpTest, GeneratePropertiesFile)
 {
-    // Basic test
-    const string prop_file_name        = "test.properties";
-    ScsiDump::inquiry_info_t test_data = {
+	// Basic test
+	auto filename = CreateTempFile(0);
+	ScsiDump::inquiry_info_t test_data = {
         .vendor = "PISCSI", .product = "TEST PRODUCT", .revision = "REV1", .sector_size = 1000, .capacity = 100};
-    TestableScsidump::PublicGeneratePropertiesFile("test", test_data);
+	test_data.GeneratePropertiesFile(filename);
 
     string expected_str = "{\n"
                           "   \"vendor\": \"PISCSI\",\n"
                           "   \"product\": \"TEST PRODUCT\",\n"
                           "   \"revision\": \"REV1\",\n"
-                          "   \"block_size\": \"1000\",\n}"
+                          "   \"block_size\": \"1000\"\n}"
                           "\n";
-    EXPECT_EQ(ReadTempFileToString(prop_file_name), expected_str);
+    EXPECT_EQ(expected_str, ReadTempFileToString(filename));
 
     // Long string test
+    filename = CreateTempFile(0);
     test_data = {.vendor      = "01234567",
                  .product     = "0123456789ABCDEF",
                  .revision    = "0123",
                  .sector_size = UINT32_MAX,
                  .capacity    = UINT64_MAX};
-    TestableScsidump::PublicGeneratePropertiesFile("test", test_data);
+    test_data.GeneratePropertiesFile(filename);
 
     expected_str = "{\n"
                    "   \"vendor\": \"01234567\",\n"
                    "   \"product\": \"0123456789ABCDEF\",\n"
                    "   \"revision\": \"0123\",\n"
-                   "   \"block_size\": \"4294967295\",\n"
+                   "   \"block_size\": \"4294967295\"\n"
                    "}\n";
-    EXPECT_EQ(ReadTempFileToString(prop_file_name), expected_str);
+    EXPECT_EQ(expected_str, ReadTempFileToString(filename));
+    remove(filename);
 
     // Empty data test
+    filename = CreateTempFile(0);
     test_data = {.vendor = "", .product = "", .revision = "", .sector_size = 0, .capacity = 0};
-    TestableScsidump::PublicGeneratePropertiesFile("test", test_data);
+    test_data.GeneratePropertiesFile(filename);
 
     expected_str = "{\n"
                    "   \"vendor\": \"\",\n"
                    "   \"product\": \"\",\n"
                    "   \"revision\": \"\",\n"
-                   "   \"block_size\": \"0\",\n"
+                   "   \"block_size\": \"0\"\n"
                    "}\n";
-    EXPECT_EQ(ReadTempFileToString(prop_file_name), expected_str);
+    EXPECT_EQ(expected_str, ReadTempFileToString(filename));
+    remove(filename);
 }
