04d6cb96d986a6d3ed9d5e0f5d8c29ea1a9e5371
Uwe Seimet <48174652+uweseimet@users.noreply.github.com>
2023-11-14 15:13:45 +0100

    Add initial IDE setup (#1326)

--- cpp/launch_sudo.sh
+++
@@ -1,3 +0,0 @@
-# This is used for debugging. VisualStudio code will call this file when launching 
-# the debugger, instead of directly calling GDB. That way we can add the pkexec
-sudo /usr/bin/gdb "$@"
