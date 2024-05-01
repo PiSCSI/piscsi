adfe0e4823527854020ff3be35d46d442d66d1b7
Daniel Markstedt <markstedt@gmail.com>
2024-05-01 16:10:39 +0900

    Update version for development

--- cpp/shared/piscsi_version.cpp
+++ cpp/shared/piscsi_version.cpp
@@ -13,8 +13,8 @@
 
 // The following should be updated for each release
 const int piscsi_major_version = 24; // Last two digits of year
-const int piscsi_minor_version =  4; // Month
-const int piscsi_patch_version =  1; // Patch number - increment for each update
+const int piscsi_minor_version =  5; // Month
+const int piscsi_patch_version = -1; // Patch number - increment for each update
 
 using namespace std;
 
