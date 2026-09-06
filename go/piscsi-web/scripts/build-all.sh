12b1b1db6f62ee6689d5dde2b79a98578e40a816
Daniel Markstedt <daniel@mindani.net>
2026-09-06 10:30:44 +0200

    touch up platform architecture build messages

--- go/piscsi-web/scripts/build-all.sh
+++ go/piscsi-web/scripts/build-all.sh
@@ -20,13 +20,13 @@ echo "🔨 Building for native platform (x86_64)..."
 make build
 echo ""
 
-# Build for Raspberry Pi ARM64 (Pi 4/5)
-echo "🔨 Building for ARM64 (Raspberry Pi 4/5)..."
+# Build for Raspberry Pi ARM64 (Pi 3/4/5)
+echo "🔨 Building for ARM64..."
 make build-linux-arm64
 echo ""
 
-# Build for Raspberry Pi ARMv7 (Pi 2/3)
-echo "🔨 Building for ARMv7 (Raspberry Pi 2/3)..."
+# Build for Raspberry Pi ARMv7 (Pi 2/early 3)
+echo "🔨 Building for ARMv7..."
 make build-linux-armv7
 echo ""
 
